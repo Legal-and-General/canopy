5b590400eb69aae6def7a21c462783449cddf261
Andrew Polhill <thatguynamedandy@users.noreply.github.com>
2019-06-12 09:28:36 +0100

    Storybook perf (#26)
    * fix(netlify): increase storybook performance to prevent netlify timeout
    
    * docs(storybook): moves to a manual per story include
    
    The require.context approach which dynamically found story files was timing out on netlify, this
    approach isn't ideal but it does work for the interim.

--- .storybook/config.js
+++ .storybook/config.js
@@ -1,17 +1,13 @@
 import { addDecorator, configure } from '@storybook/angular';
 import { withA11y } from '@storybook/addon-a11y';
-import '!style-loader!css-loader!sass-loader!../projects/canopy/src/styles/styles.scss';
-
 
 addDecorator(withA11y);
 
 function loadStories() {
-  const req = require.context(
-    '../projects/canopy/src/',
-    true,
-    /\.stories\.ts$/
-  );
-  req.keys().forEach(filename => req(filename));
+  require('!style-loader!css-loader!sass-loader!../projects/canopy/src/styles/styles.scss');
+  require('../projects/canopy/src/lib/button/button.stories');
+  require('../projects/canopy/src/styles/typography.stories');
+
 }
 
 configure(loadStories, module);
