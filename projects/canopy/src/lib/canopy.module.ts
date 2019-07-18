5627327f5bdf86b738b24363a25347ef8b366ac1
Elena Garrone <elena.15@hotmail.it>
2019-07-18 10:15:51 +0100

    fix: update wrong import/export for FeatureToggle in canopy module (#42)

--- projects/canopy/src/lib/canopy.module.ts
+++ projects/canopy/src/lib/canopy.module.ts
@@ -2,11 +2,11 @@ import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
 import { ButtonComponent } from './button/button.component';
 import { CanopyComponent } from './canopy.component';
-import { FeatureToggleDirective } from './feature-toggle/feature-toggle.directive';
+import { FeatureToggleModule } from './feature-toggle/feature-toggle.module';
 
 @NgModule({
-  declarations: [ButtonComponent, CanopyComponent, FeatureToggleDirective],
+  declarations: [ButtonComponent, CanopyComponent, FeatureToggleModule],
   imports: [CommonModule],
-  exports: [ButtonComponent, CanopyComponent, FeatureToggleDirective]
+  exports: [ButtonComponent, CanopyComponent, FeatureToggleModule]
 })
 export class CanopyModule {}
