a583c26888d208e2654cfd82504decf0b57778a0
Andrew Polhill <thatguynamedandy@users.noreply.github.com>
2019-10-24 15:41:18 +0100

    fix(public-api): adds missing public api components

--- projects/canopy/src/lib/card/card.module.ts
+++ projects/canopy/src/lib/card/card.module.ts
@@ -1,8 +1,11 @@
+import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
 
 import { LgCardComponent } from './card/card.component';
 
 @NgModule({
+  imports: [CommonModule],
+  declarations: [LgCardComponent],
   exports: [LgCardComponent]
 })
 export class LgCardModule {}
--- projects/canopy/src/public-api.ts
+++ projects/canopy/src/public-api.ts
@@ -5,12 +5,13 @@
 export * from './lib/canopy.service';
 export * from './lib/canopy.module';
 
-export * from './lib/heading/index';
 export * from './lib/button/index';
-export * from './lib/forms/index';
-
-export * from './lib/focus/index';
-
+export * from './lib/card/index';
 export * from './lib/feature-toggle/index';
-
+export * from './lib/focus/index';
+export * from './lib/footer/index';
+export * from './lib/forms/index';
+export * from './lib/heading/index';
+export * from './lib/page/index';
 export * from './lib/pipes/index';
+export * from './lib/spinner/index';
