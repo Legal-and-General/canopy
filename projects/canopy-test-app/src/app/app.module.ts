24b41d9e12aff59139d9ca6b8cb357d6c653f072
Andrew Polhill <thatguynamedandy@users.noreply.github.com>
2019-11-19 10:17:51 +0000

    test(canopy-test-app): adds a test application
    To be able to test the production build it is useful to have a sample project. This project is a
    result of running the comand `ng generate application canopy-test` with no modifications

---
+++ projects/canopy-test-app/src/app/app.module.ts
@@ -0,0 +1,13 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { CanopyModule } from 'canopy';
+
+import { AppComponent } from './app.component';
+@NgModule({
+  declarations: [AppComponent],
+  imports: [BrowserModule, CanopyModule],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule {}
