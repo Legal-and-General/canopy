4f76712932a167e47344481de5312cc8fbc2c794
Andrew Polhill <thatguynamedandy@users.noreply.github.com>
2019-11-19 22:57:27 +0000

    test(canopy-test-app): removes canopy-test-app dist from git

--- projects/canopy-test-app/src/app/app.component.spec.ts
+++ projects/canopy-test-app/src/app/app.component.spec.ts
@@ -1,4 +1,5 @@
-import { TestBed, async } from '@angular/core/testing';
+import { async, TestBed } from '@angular/core/testing';
+
 import { AppComponent } from './app.component';
 
 describe('AppComponent', () => {
