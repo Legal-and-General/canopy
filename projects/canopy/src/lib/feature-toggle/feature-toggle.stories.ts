b3070abbd6c8094453918c696e776c9723195b4f
Henrique Zago <henriquezago@users.noreply.github.com>
2019-12-19 12:59:22 +0000

    docs(feature-toggle): improves feature toggle story
    Improves Feature Toggle story with LgCardComponent.
    
    fix 118

--- projects/canopy/src/lib/feature-toggle/feature-toggle.stories.ts
+++ projects/canopy/src/lib/feature-toggle/feature-toggle.stories.ts
@@ -1,66 +1,49 @@
 import { storiesOf } from '@storybook/angular';
 import { of } from 'rxjs';
-import { LgFeatureToggleOptions } from './feature-toggle.interface';
 
+import { LgCardComponent } from '../card';
+import { LgFeatureToggleOptions } from './feature-toggle.interface';
 import { LgFeatureToggleModule } from './feature-toggle.module';
 import { notes } from './feature-toggle.notes';
 
-const stories = storiesOf('Modules', module);
+const stories = storiesOf('Directives', module);
 const options: LgFeatureToggleOptions = {
   // disable undefined feature e.g. Feature 4 in the following story
   defaultHide: true
 };
 
-stories.add(
-  'Feature Toggle',
-  () => ({
-    moduleMetadata: {
-      imports: [
-        LgFeatureToggleModule.forRoot(
-          {
-            useFactory: () =>
-              of({
-                firstFeature: true,
-                secondFeature: false,
-                thirdFeature: true
-              })
-          },
-          options
-        )
-      ]
-    },
-    template: `
-  <ul>
-    <li *lgFeatureToggle="'firstFeature'" class="first">Feature 1 showing</li>
-    <li *lgFeatureToggle="'secondFeature'" class="second">Feature 2 not showing</li>
-    <li *lgFeatureToggle="'thirdFeature'" class="third">Feature 3 showing</li>
-    <li *lgFeatureToggle="'fourthFeature'" class="third">Feature 4 not showing</li>
-  </ul>
-  `,
-    styles: [
-      `
-    li {
-      margin-top: 20px;
-      width: 80px;
-      height: 80px;
-      color: white;
-    }
-
-    .first {
-      background-color: darkred;
-    }
-
-    .second {
-      background-color: darkblue;
-    }
-
-    .third {
-      background-color: darkgreen;
-    }
+stories
+  .addParameters({
+    backgrounds: [{ name: 'default', value: '#0076d6', default: true }]
+  })
+  .add(
+    'Feature Toggle',
+    () => ({
+      moduleMetadata: {
+        declarations: [LgCardComponent],
+        imports: [
+          LgFeatureToggleModule.forRoot(
+            {
+              useFactory: () =>
+                of({
+                  firstFeature: true,
+                  secondFeature: false,
+                  thirdFeature: true,
+                  fourthFeature: false
+                })
+            },
+            options
+          )
+        ]
+      },
+      template: `
+  <lg-card *lgFeatureToggle="'firstFeature'">Feature 1 showing</lg-card>
+  <lg-card *lgFeatureToggle="'secondFeature'">Feature 2 not showing</lg-card>
+  <lg-card *lgFeatureToggle="'thirdFeature'">Feature 3 showing</lg-card>
+  <lg-card *lgFeatureToggle="'fourthFeature'">Feature 4 not showing</lg-card>
   `
-    ]
-  }),
-  {
-    notes: { markdown: notes }
-  }
-);
+    }),
+    {
+      notes: { markdown: notes }
+    }
+  );
