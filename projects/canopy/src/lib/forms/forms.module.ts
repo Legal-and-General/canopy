07a19656efde76608468c8f2da7bb701d0895cbb
Elena Garrone <elenagarrone@users.noreply.github.com>
2020-02-05 17:23:17 +0000

    fix(toggle-switch): add variables for toggle-switch
    update scss file to calulate automatically the dimensions of the switch

--- projects/canopy/src/lib/forms/forms.module.ts
+++ projects/canopy/src/lib/forms/forms.module.ts
@@ -1,11 +1,11 @@
 import { NgModule } from '@angular/core';
 
-import { LgToggleModule } from './toggle/toggle.module';
 import { LgHintModule } from './hint/hint.module';
 import { LgInputModule } from './input/input.module';
 import { LgLabelModule } from './label/label.module';
 import { LgRadioModule } from './radio/radio.module';
 import { LgSelectModule } from './select/select.module';
+import { LgToggleModule } from './toggle/toggle.module';
 
 @NgModule({
   exports: [
