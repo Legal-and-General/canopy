8ce2b66d9f1856502a5f8f2f8504201d1846297d
Elena Garrone <elenagarrone@users.noreply.github.com>
2020-03-31 15:06:45 +0100

    fix(accordion): pushed some changes by mistake
    this reverts the changes made by mistake to the accordion

--- projects/canopy/src/lib/accordion/accordion.stories.ts
+++ projects/canopy/src/lib/accordion/accordion.stories.ts
@@ -1,46 +1,8 @@
-import { Component, Input, NgModule } from '@angular/core';
 import { select, withKnobs } from '@storybook/addon-knobs';
 import { storiesOf } from '@storybook/angular';
-import { BreadcrumbVariant, LgBreadcrumbModule } from '../breadcrumb';
 import { LgButtonModule } from '../button';
-import { LgIconModule, LgIconRegistry } from '../icon';
-import * as iconSet from '../icon/icons.interface';
-import { LgMarginDirective } from '../margin';
 import { LgAccordionModule } from './accordion.module';
 import { notes } from './accordion.notes';
-
-@Component({
-  selector: 'lg-accordion-story-component',
-  template: `
-    <lg-accordion [headingLevel]="headingLevel">
-      <lg-accordion-item>
-        <lg-accordion-panel-heading [headingLevel]="headingLevel">
-          <lg-icon name="news" class="lg-margin__right--sm"></lg-icon>
-          Item 1
-        </lg-accordion-panel-heading>
-        <p>
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-          eiusmod tempor incididunt ut labore et dolore magna aliqua.
-        </p>
-      </lg-accordion-item>
-    </lg-accordion>
-  `
-})
-export class LgAccordionStoryComponent {
-  @Input() headingLevel: 3;
-}
-
-@NgModule({
-  imports: [LgIconModule, LgAccordionModule],
-  exports: [LgAccordionStoryComponent],
-  declarations: [LgAccordionStoryComponent],
-  providers: []
-})
-export class LgAccordionStoryModule {
-  constructor(private registry: LgIconRegistry) {
-    this.registry.registerIcons([iconSet.lgIconNews]);
-  }
-}
 
 const stories = storiesOf('Components', module).addDecorator(withKnobs);
 
@@ -52,11 +14,29 @@ stories
     'Accordion',
     () => ({
       moduleMetadata: {
-        imports: [LgAccordionStoryModule]
+        imports: [LgAccordionModule, LgButtonModule]
       },
       template: `
-        <lg-accordion-story-component [headingLevel]="3"></lg-accordion-story-component>
-      `
+        <lg-accordion [headingLevel]="headingLevel">
+          <lg-accordion-item>
+            <lg-accordion-panel-heading [headingLevel]="headingLevel">Item 1</lg-accordion-panel-heading>
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
+              tempor incididunt ut labore et dolore magna aliqua.</p>
+          </lg-accordion-item>
+          <lg-accordion-item>
+            <lg-accordion-panel-heading [headingLevel]="headingLevel">Item 2</lg-accordion-panel-heading>
+            <p>Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi
+              ut aliquip ex ea commodo consequat. Duis aute irure dolor in
+              reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
+              pariatur.</p>
+            <button lg-button lgMarginTop="sm" variant="solid-primary">
+              Test primary button
+            </button>
+          </lg-accordion-item>
+        </lg-accordion>`,
+      props: {
+        headingLevel: select('headingLevel', [1, 2, 3, 4, 5, 6], 2)
+      }
     }),
     {
       notes: {
