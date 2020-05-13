840497f5bd3fce51a77ecac3ed036e4431b60d6a
Andrew Polhill <thatguynamedandy@users.noreply.github.com>
2020-05-13 16:21:07 +0100

    docs(grid): fixes incorrectly named directives
    The notes for grid did not match the correct names of the directives.

--- projects/canopy/src/lib/grid/grid.notes.ts
+++ projects/canopy/src/lib/grid/grid.notes.ts
@@ -18,10 +18,10 @@ There are three directives included in this module, which can all be added to Ca
 A simple one column responsive grid which expands to two columns for larger screen sizes could be created with the following markup:
 
 ~~~html
-<div lgGridContainer>
-  <div lgGridRow>
-    <div lgGridCol=12 lgGridColMd=6>Column 1</div>
-    <div lgGridCol=12 lgGridColMd=6>Column 2</div>
+<div lgContainer>
+  <div lgRow>
+    <div lgCol=12 lgColMd=6>Column 1</div>
+    <div lgCol=12 lgColMd=6>Column 2</div>
   </div>
 </div>
 ~~~
@@ -31,14 +31,14 @@ A simple one column responsive grid which expands to two columns for larger scre
 
 | Name | Description | Type | Default | Required |
 |------|-------------|:----:|:-----:|:-----:|
-| \`\`lgGridContainer\`\` | Adds the grid container class to the element | null | null | No |
-| \`\`lgGridRow\`\` | Adds the row class to the element | null | null | No |
-| \`\`lgGridCol\`\` | Specifies the number of columns to fill in a 12 column layout | number (1-12) | null | Yes |
-| \`\`lgGridColMd\`\` | Specifies the number of columns to fill in a 12 column layout on a medium sized screen | number (1-12) | null | Yes |
-| \`\`lgGridColLg\`\` | Specifies the number of columns to fill in a 12 column layout on a large sized screen | number (1-12) | null | Yes |
-| \`\`lgGridColOffset\`\` | Specifies the number of columns to offset in a 12 column layout | number (0-11) | null | Yes |
-| \`\`lgGridColMdOffset\`\` | Specifies the number of columns to offset fill in a 12 column layout on a medium sized screen | number (0-11) | null | Yes |
-| \`\`lgGridColLgOffset\`\` | Specifies the number of columns to offset fill in a 12 column layout on a large sized screen | number (0-11) | null | Yes |
+| \`\`lgContainer\`\` | Adds the grid container class to the element | null | null | No |
+| \`\`lgRow\`\` | Adds the row class to the element | null | null | No |
+| \`\`lgCol\`\` | Specifies the number of columns to fill in a 12 column layout | number (1-12) | null | Yes |
+| \`\`lgColMd\`\` | Specifies the number of columns to fill in a 12 column layout on a medium sized screen | number (1-12) | null | Yes |
+| \`\`lgColLg\`\` | Specifies the number of columns to fill in a 12 column layout on a large sized screen | number (1-12) | null | Yes |
+| \`\`lgColOffset\`\` | Specifies the number of columns to offset in a 12 column layout | number (0-11) | null | Yes |
+| \`\`lgColMdOffset\`\` | Specifies the number of columns to offset fill in a 12 column layout on a medium sized screen | number (0-11) | null | Yes |
+| \`\`lgColLgOffset\`\` | Specifies the number of columns to offset fill in a 12 column layout on a large sized screen | number (0-11) | null | Yes |
 
 ## Using only the SCSS files
 
