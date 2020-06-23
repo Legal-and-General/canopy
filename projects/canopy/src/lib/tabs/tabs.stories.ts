fa7617369419b640dedd3c5a460d70c63bb622b5
Andrew Polhill <thatguynamedandy@users.noreply.github.com>
2020-06-23 11:43:40 +0100

    docs(tabs): updates story to CSF format

--- projects/canopy/src/lib/tabs/tabs.stories.ts
+++ projects/canopy/src/lib/tabs/tabs.stories.ts
@@ -1,54 +1,58 @@
 import { action } from '@storybook/addon-actions';
 import { object, select, withKnobs } from '@storybook/addon-knobs';
-import { storiesOf } from '@storybook/angular';
-import { CanopyModule } from '../canopy.module';
+import { moduleMetadata } from '@storybook/angular';
+
+import { LgMarginModule } from '../spacing';
+import { LgTabsModule } from './tabs.module';
 import { notes } from './tabs.notes';
 
-const stories = storiesOf('Components', module).addDecorator(withKnobs);
+export default {
+  title: 'Components/Tabs',
+  parameters: {
+    decorators: [
+      withKnobs,
+      moduleMetadata({
+        imports: [LgTabsModule, LgMarginModule]
+      })
+    ],
+    'in-dsm': {
+      id: '5ec50da107ffe67b65c12ef7'
+    },
+    notes: {
+      markdown: notes
+    }
+  }
+};
+
+export const standard = () => ({
+  template: `
+  <lg-tabs [headingLevel]="headingLevel"
+    label="Annuities"
+    (tabEvent)="tabEvent($event)"
+    [lgMarginTop]="'xxs'"
+    [lgMarginRight]="'none'"
+    [lgMarginBottom]="'none'"
+    [lgMarginLeft]="'none'"
+  >
+    <lg-tab-item *ngFor="let tab of tabs">
+      <lg-tab-item-heading>{{ tab.header }}</lg-tab-item-heading>
+      <lg-tab-item-content>
+        <div class="lg-tabs__content-section">{{ tab.content }}</div>
+      </lg-tab-item-content>
+    </lg-tab-item>
+  </lg-tabs>
+  `,
+  props: {
+    tabs: object('Tabs', getDefultTabsContent()),
+    headingLevel: select('headingLevel', [1, 2, 3, 4, 5, 6], 2),
+    tabEvent: action('tab selected')
+  }
+});
 
 interface TabStoryItem {
   header: string;
   content: string;
 }
-stories.add(
-  'Tabs',
-  () => {
-    require('!style-loader!css-loader!sass-loader!./../../../../../.storybook/full-screen.css');
-
-    return {
-      moduleMetadata: {
-        imports: [CanopyModule]
-      },
-      template: `
-        <lg-tabs [headingLevel]="headingLevel"
-          label="Annuities"
-          (tabEvent)="tabEvent($event)"
-          [lgMarginTop]="'xxs'"
-          [lgMarginRight]="'none'"
-          [lgMarginBottom]="'none'"
-          [lgMarginLeft]="'none'"
-        >
-          <lg-tab-item *ngFor="let tab of tabs">
-            <lg-tab-item-heading>{{ tab.header }}</lg-tab-item-heading>
-            <lg-tab-item-content>
-              <div class="lg-tabs__content-section">{{ tab.content }}</div>
-            </lg-tab-item-content>
-          </lg-tab-item>
-        </lg-tabs>
-      `,
-      props: {
-        tabs: object('Tabs', getDefultTabsContent()),
-        headingLevel: select('headingLevel', [1, 2, 3, 4, 5, 6], 2),
-        tabEvent: action('tab selected')
-      }
-    };
-  },
-  {
-    notes: {
-      markdown: notes
-    }
-  }
-);
 
 function getDefultTabsContent(): TabStoryItem[] {
   return [
