f369c3290527940f0ee7dcfb7b2e4e6af742a0f5
Andrew Polhill <thatguynamedandy@users.noreply.github.com>
2020-06-23 11:38:32 +0100

    docs(alert): updates story to CSF format

--- projects/canopy/src/lib/alert/alert.stories.ts
+++ projects/canopy/src/lib/alert/alert.stories.ts
@@ -1,41 +1,42 @@
 import { boolean, select, text, withKnobs } from '@storybook/addon-knobs';
-import { storiesOf } from '@storybook/angular';
+import { moduleMetadata } from '@storybook/angular';
 
 import { LgAlertModule } from '../alert/alert.module';
 import { notes } from './alert.notes';
 
-const stories = storiesOf('Components', module).addDecorator(withKnobs);
-
-stories
-  .addParameters({
-    backgrounds: [{ name: 'default', value: '#ffffff', default: true }]
-  })
-  .add(
-    'Alert',
-    () => ({
-      moduleMetadata: {
+export default {
+  title: 'Components/Alert',
+  parameters: {
+    decorators: [
+      withKnobs,
+      moduleMetadata({
         imports: [LgAlertModule]
-      },
-      template: `
-      <lg-alert
-        [showIcon]="showIcon"
-        [variant]="variant">
-        {{alertContent}} Here is some <a href="#"> link text</a>.
-      </lg-alert>
-    `,
-      props: {
-        alertContent: text('content', 'This is an alert.'),
-        showIcon: boolean('show icon', true),
-        variant: select(
-          'variant',
-          ['generic', 'info', 'success', 'warning', 'error'],
-          'generic'
-        )
-      }
-    }),
-    {
-      notes: {
-        markdown: notes
-      }
+      })
+    ],
+    'in-dsm': {
+      id: '5ec504bc07ffe609bec12b76'
+    },
+    notes: {
+      markdown: notes
     }
-  );
+  }
+};
+
+export const standard = () => ({
+  template: `
+    <lg-alert
+      [showIcon]="showIcon"
+      [variant]="variant">
+      {{alertContent}} Here is some <a href="#"> link text</a>.
+    </lg-alert>
+  `,
+  props: {
+    alertContent: text('content', 'This is an alert.'),
+    showIcon: boolean('show icon', true),
+    variant: select(
+      'variant',
+      ['generic', 'info', 'success', 'warning', 'error'],
+      'generic'
+    )
+  }
+});
