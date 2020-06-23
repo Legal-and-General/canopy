3823f106da861aba5e65cd4320accf6b9b050789
Andrew Polhill <thatguynamedandy@users.noreply.github.com>
2020-06-23 11:58:49 +0100

    docs(header): updates story to CSF format

--- projects/canopy/src/lib/header/header.stories.ts
+++ projects/canopy/src/lib/header/header.stories.ts
@@ -1,42 +1,37 @@
 import { text, withKnobs } from '@storybook/addon-knobs';
-import { storiesOf } from '@storybook/angular';
+import { moduleMetadata } from '@storybook/angular';
 
 import { LgHeaderComponent } from '../header/header.component';
 import { notes } from './header.notes';
 
-const stories = storiesOf('Components', module).addDecorator(withKnobs);
+export default {
+  title: 'Header',
+  parameters: {
+    decorators: [
+      withKnobs,
+      moduleMetadata({
+        declarations: [LgHeaderComponent]
+      })
+    ],
+    notes: {
+      markdown: notes
+    }
+  }
+};
 
 const groupId = 'header';
 
-stories
-  .addParameters({
-    backgrounds: [{ name: 'default', value: '#f5f7fa', default: true }]
-  })
-  .add(
-    'Header',
-    () => {
-      require('!style-loader!css-loader!sass-loader!../../../../../.storybook/full-screen.css');
-      return {
-        moduleMetadata: {
-          declarations: [LgHeaderComponent]
-        },
-        template: `
-        <header lg-header [logo]="logo" [logoAlt]="logoAlt" [logoHref]="logoHref"></header>
-      `,
-        props: {
-          logo: text(
-            'logo',
-            'https://placeholder.com/wp-content/uploads/2018/10/placeholder.com-logo3.png',
-            groupId
-          ),
-          logoAlt: text('logoAlt', 'Company name', groupId),
-          logoHref: text('logoHref', 'https://storybook.js.org', groupId)
-        }
-      };
-    },
-    {
-      notes: {
-        markdown: notes
-      }
-    }
-  );
+export const Default = () => ({
+  template: `
+    <header lg-header [logo]="logo" [logoAlt]="logoAlt" [logoHref]="logoHref"></header>
+  `,
+  props: {
+    logo: text(
+      'logo',
+      'https://placeholder.com/wp-content/uploads/2018/10/placeholder.com-logo3.png',
+      groupId
+    ),
+    logoAlt: text('logoAlt', 'Company name', groupId),
+    logoHref: text('logoHref', 'https://storybook.js.org', groupId)
+  }
+});
