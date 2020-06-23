83dbcbca76f807aa66a72704d8b8c32e7279437f
Andrew Polhill <thatguynamedandy@users.noreply.github.com>
2020-06-23 11:40:44 +0100

    docs(footer): updates story to CSF format

--- projects/canopy/src/lib/footer/footer.stories.ts
+++ projects/canopy/src/lib/footer/footer.stories.ts
@@ -1,14 +1,29 @@
 import { object, text, withKnobs } from '@storybook/addon-knobs';
-import { storiesOf } from '@storybook/angular';
+import { moduleMetadata } from '@storybook/angular';
 
 import { action } from '@storybook/addon-actions';
 
 import { LgFooterComponent } from '../footer/footer.component';
 import { notes } from './footer.notes';
 
-const stories = storiesOf('Components', module).addDecorator(withKnobs);
-
-const groupId = 'footer';
+export default {
+  title: 'Components/Footer',
+  excludeStories: ['primaryLinks', 'secondaryLinks'],
+  parameters: {
+    decorators: [
+      withKnobs,
+      moduleMetadata({
+        declarations: [LgFooterComponent]
+      })
+    ],
+    'in-dsm': {
+      id: '5ec4f36b45894b0873022460'
+    },
+    notes: {
+      markdown: notes
+    }
+  }
+};
 
 export const primaryLinks = [
   { text: 'My account', href: 'https://app.somecompany.com' },
@@ -23,48 +38,31 @@ export const secondaryLinks = [
   { text: 'Legal and regulatory', href: 'https://somecompany.com/legal' }
 ];
 
-stories
-  .addParameters({
-    backgrounds: [{ name: 'default', value: '#f5f7fa', default: true }]
-  })
-  .add(
-    'Footer',
-    () => {
-      require('!style-loader!css-loader!sass-loader!../../../../../.storybook/full-screen.css');
+const groupId = 'footer';
 
-      return {
-        moduleMetadata: {
-          declarations: [LgFooterComponent]
-        },
-        template: `
-        <footer lg-footer
-          [copyright]="copyright"
-          [logo]="logo"
-          [logoAlt]="logoAlt"
-          [primaryLinks]="primaryLinks"
-          [secondaryLinks]="secondaryLinks"
-          (primaryLinkClicked)="primaryLinkClicked($event)"
-          (secondaryLinkClicked)="secondaryLinkClicked($event)">
-        </footer>
-      `,
-        props: {
-          logo: text(
-            'logo',
-            'https://placeholder.com/wp-content/uploads/2018/10/placeholder.com-logo3.png',
-            groupId
-          ),
-          logoAlt: text('logoAlt', 'Company name', groupId),
-          copyright: text('copyright', '© Some Company plc 2018', groupId),
-          secondaryLinks: object('secondaryLinks', secondaryLinks, groupId),
-          primaryLinks: object('primaryLinks', primaryLinks, groupId),
-          primaryLinkClicked: action('primaryLinkClicked'),
-          secondaryLinkClicked: action('secondaryLinkClicked')
-        }
-      };
-    },
-    {
-      notes: {
-        markdown: notes
-      }
-    }
-  );
+export const standard = () => ({
+  template: `
+    <footer lg-footer
+      [copyright]="copyright"
+      [logo]="logo"
+      [logoAlt]="logoAlt"
+      [primaryLinks]="primaryLinks"
+      [secondaryLinks]="secondaryLinks"
+      (primaryLinkClicked)="primaryLinkClicked($event)"
+      (secondaryLinkClicked)="secondaryLinkClicked($event)">
+    </footer>
+  `,
+  props: {
+    logo: text(
+      'logo',
+      'https://placeholder.com/wp-content/uploads/2018/10/placeholder.com-logo3.png',
+      groupId
+    ),
+    logoAlt: text('logoAlt', 'Company name', groupId),
+    copyright: text('copyright', '© Some Company plc 2018', groupId),
+    secondaryLinks: object('secondaryLinks', secondaryLinks, groupId),
+    primaryLinks: object('primaryLinks', primaryLinks, groupId),
+    primaryLinkClicked: action('primaryLinkClicked'),
+    secondaryLinkClicked: action('secondaryLinkClicked')
+  }
+});
