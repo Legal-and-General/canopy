e4d8dcbac2b9059b90093d85758c491bc408a4a0
Andrew Polhill <thatguynamedandy@users.noreply.github.com>
2020-06-24 22:18:12 +0100

    docs(storybook): adds basic theme control

--- .storybook/addons.js
+++ .storybook/addons.js
@@ -6,5 +6,11 @@ import '@storybook/addon-viewport/register';
 import '@storybook/addon-a11y/register';
 import '@storybook/addon-storysource/register';
 import { registerDsm } from '@invisionapp/dsm-storybook/register';
+import { addons } from '@storybook/addons';
+import canopyTheme from './canopy-theme';
+
+addons.setConfig({
+  theme: canopyTheme
+});
 
 registerDsm(process.env.STORYBOOK_DSM);
