db46ff6008a12c4352faa0c7a04288b43ae6fab2
Andrew Polhill <2196085+thatguynamedandy@users.noreply.github.com>
2020-10-02 16:04:37 +0100

    feat(typography): adds strong modifier to font style classes
    Font style classes now have the addition of a `--strong` modifier. Not currently used in any
    components but added to allow more flexible typography within applications
    
    Co-authored-by: Alex Canning <56020713+alexcanning@users.noreply.github.com>

--- projects/canopy/src/styles/typography.notes.ts
+++ projects/canopy/src/styles/typography.notes.ts
@@ -11,18 +11,35 @@ In addition to the native styling the following utility classes are exposed.
 
 ~~~html
 .lg-font-size-7
+.lg-font-size-7--strong
+
 .lg-font-size-6
+.lg-font-size-6--strong
+
 .lg-font-size-5
+.lg-font-size-5--strong
+
 .lg-font-size-4
+.lg-font-size-4--strong
+
 .lg-font-size-3
+.lg-font-size-3--strong
+
 .lg-font-size-2
+.lg-font-size-2--strong
+
 .lg-font-size-1
+.lg-font-size-1--strong
+
 .lg-font-size-0-8
+
 .lg-font-size-0-6
 ~~~
 
 \`.lg-font-size-1\` is the base font size that is used to style native elements like p and span, as the numbers increase so
-does the font size up to the largest \`.lg-font-size-7.\` There are two additional styles which are smaller \`.lg-font-size-0-8\`
+does the font size up to the largest \`.lg-font-size-7.\` Each of the primary font styles also have a \`--strong\` modifier.
+
+There are two additional styles which are smaller \`.lg-font-size-0-8\`
 and \`.lg-font-size-0-6\`. The utility classes are particularly useful for when you want to decouple the semantics and styling,
 for example making an h1 look like an h3.
 
