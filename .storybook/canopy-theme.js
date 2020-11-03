99a2e48c4e1707506b07a15ed9bc08fffa40e7c9
Andrew Polhill <2196085+thatguynamedandy@users.noreply.github.com>
2020-11-03 16:49:38 +0000

    docs: adds brand logo to storybook

--- .storybook/canopy-theme.js
+++ .storybook/canopy-theme.js
@@ -3,5 +3,6 @@ import { create } from '@storybook/theming/create';
 export default create({
   base: 'light',
   brandTitle: 'Canopy',
-  brandUrl: 'https://github.com/Legal-and-General/canopy'
+  brandUrl: 'https://github.com/Legal-and-General/canopy',
+  brandImage: 'legal-and-general-logo.png',
 });
