155299f76b97da013a8ef5e9aff6eadd59696d95
Elena Garrone <8397116+elenagarrone@users.noreply.github.com>
2021-03-25 08:41:31 +0000

    feat(links): update links to new design
    BREAKING CHANGE:
    New themed link styles have been introduced.
    In most scenarios these link styles should just work with pre-existing Canopy components.
    Please do check for individual instances within your application.
    If you need to revert to the original link style use either the `lg-unstyled-link` class on
    the element or the `lg-unstyled-link` mixin at the top of the selector.
    
    e.g.
    ```scss
    a {
      @include lg-unstyled-link
    
      /* ...other styles */
    }
    
    BREAKING CHANGE:
    Rename `--link-focus-background-color` to `--link-focus-bg-color`.

--- projects/canopy/src/styles/utils.notes.ts
+++ projects/canopy/src/styles/utils.notes.ts
@@ -8,4 +8,5 @@ Provides available utils classes
 | Class | Description |
 |------|-------------|
 | \`\`lg-visually-hidden\`\` | Hides information intended only for screen readers from the layout of the rendered page. |
+| \`\`lg-unstyled-link\`\` | Revert the links styles to a more default style. |
 `;
