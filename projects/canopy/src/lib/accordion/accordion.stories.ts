4a5f5fa0d3e4ece3bbbe51d70931ef3c1284844c
Elena Garrone <8397116+elenagarrone@users.noreply.github.com>
2021-03-25 08:41:31 +0000

    feat(links): update links to new design
    BREAKING CHANGE:
    New themed link styles have been introduced.
    In most scenarios these link styles should just work with pre-existing Canopy components.
    Please do check for individual instances within your application.
    If you need to revert to the original link style use either the `lg-unstyled-link` class on
    the element or the `lg-unstyled-link` mixin at the top of the selector.
    
    e.g.
    ```scss
    a {
      @include lg-unstyled-link
    
      /* ...other styles */
    }
    
    BREAKING CHANGE:
    Rename `--link-focus-background-color` to `--link-focus-bg-color`.

--- projects/canopy/src/lib/accordion/accordion.stories.ts
+++ projects/canopy/src/lib/accordion/accordion.stories.ts
@@ -39,7 +39,7 @@ const accordionItems = `
 
   <p>Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi
     ut aliquip ex ea commodo consequat. Duis aute irure dolor in
-    reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
+    <a href="#">reprehenderit</a> in voluptate velit esse cillum dolore eu fugiat nulla
     pariatur.</p>
   <button lg-button lgMarginTop="sm" variant="solid-primary">
     Test primary button
--- projects/canopy/src/styles/mixins.notes.ts
+++ projects/canopy/src/styles/mixins.notes.ts
@@ -17,10 +17,30 @@ Sets the focus outline inside the element, by using multiple box shadows.
 
 Sets the focus outline outside the element, by using box shadows.
 
+### lg-link($default-color, $hover-color, $visited-color, $active-color, $active-bg-color, $focus-color, $focus-bg-color)
+\`\`$default-color\`\`: the default color of the element (default value: \`\`var(--link-color)\`\`).
+
+\`\`$hover-color\`\`: the hover color of the element (default value: \`\`var(--link-hover-color)\`\`).
+
+\`\`$visited-color\`\`: the visited color of the element (default value: \`\`var(--link-visited-color)\`\`).
+
+\`\`$active-color\`\`: the active color of the element (default value: \`\`var(--link-active-color)\`\`).
+
+\`\`$active-bg-color\`\`: the active background color of the element (default value: \`\`var(--link-active-bg-color)\`\`).
+
+\`\`$focus-color\`\`: the focus color color of the element (default value: \`\`var(--link-focus-color)\`\`).
+
+\`\`$focus-bg-color\`\`: the focus background color of the element (default value: \`\`var(--link-focus-bg-color)\`\`).
+
+Styles elements with the link style.
+
+### lg-unstyled-link
+Revert the links styles added with \`\`lg-link\`\`.
+
 ### lg-font-size($size)
 \`\`$size\`\`: The font size, '7' | '6' | '5' | '4' | '3' | '2' | '1' | '-8' | '-6' | .
 
-Style text to one of the predefined font sizes. '-8' and '-6' are the sub body font sizes.
+Styles text to one of the predefined font sizes. '-8' and '-6' are the sub body font sizes.
 
 ### lg-visually-hidden()
 Provides styles to hide information intended only for screen readers from the layout of the rendered page.
