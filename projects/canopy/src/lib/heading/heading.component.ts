e8d4457a95f9b8b22c20bdc17aa09c5394d60a50
Elena Garrone <8397116+elenagarrone@users.noreply.github.com>
2021-03-26 12:20:34 +0000

    refactor(heading): remove class input and styles
    This commit also moves the accordion styles that were in this
    component to the accordion itself.
    
    BREAKING CHANGE:
    Remove the class input from the `lg-heading` component.
    
    Fix: #225

--- projects/canopy/src/lib/heading/heading.component.ts
+++ projects/canopy/src/lib/heading/heading.component.ts
@@ -5,10 +5,8 @@ import { HeadingLevel } from './heading.interface';
 @Component({
   selector: 'lg-heading',
   templateUrl: './heading.component.html',
-  styleUrls: ['./heading.component.scss'],
   encapsulation: ViewEncapsulation.None,
 })
 export class LgHeadingComponent {
   @Input() level: HeadingLevel;
-  @Input() class?: string;
 }
