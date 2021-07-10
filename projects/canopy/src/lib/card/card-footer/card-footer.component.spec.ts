2dc67a6a6a2fb7f9fbb5199ad3a7cab8c1ffbb91
nicolaeziman <20334064+Zimann@users.noreply.github.com>
2021-07-10 12:18:18 +0300

    refactor(card): remove deprecated async

--- projects/canopy/src/lib/card/card-footer/card-footer.component.spec.ts
+++ projects/canopy/src/lib/card/card-footer/card-footer.component.spec.ts
@@ -1,4 +1,4 @@
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { ComponentFixture, TestBed, waitForAsync } from '@angular/core/testing';
 
 import { LgCardFooterComponent } from './card-footer.component';
 
@@ -6,11 +6,13 @@ describe('LgCardFooterComponent', () => {
   let component: LgCardFooterComponent;
   let fixture: ComponentFixture<LgCardFooterComponent>;
 
-  beforeEach(async(() => {
-    TestBed.configureTestingModule({
-      declarations: [LgCardFooterComponent],
-    }).compileComponents();
-  }));
+  beforeEach(
+    waitForAsync(() => {
+      TestBed.configureTestingModule({
+        declarations: [LgCardFooterComponent],
+      }).compileComponents();
+    }),
+  );
 
   beforeEach(() => {
     fixture = TestBed.createComponent(LgCardFooterComponent);
