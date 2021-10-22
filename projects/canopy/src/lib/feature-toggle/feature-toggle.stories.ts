6369b3d848d6da8abd3ff4efe64472b0dbf3e5f4
Paul Bennett <4497410+paul-request@users.noreply.github.com>
2021-10-22 10:56:28 +0100

    feat(feature-toggle): upgrade story to CSF format

--- projects/canopy/src/lib/feature-toggle/feature-toggle.stories.ts
+++ projects/canopy/src/lib/feature-toggle/feature-toggle.stories.ts
@@ -1,49 +1,89 @@
-import { storiesOf } from '@storybook/angular';
+import { Meta, Story } from '@storybook/angular';
+import { moduleMetadata } from '@storybook/angular';
 import { of } from 'rxjs';
 
-import { CanopyModule } from '../canopy.module';
-import type { LgFeatureToggleOptions } from './feature-toggle.interface';
+import { LgFeatureToggleOptions } from './feature-toggle.interface';
+import { CanopyModule } from './../canopy.module';
+import { LgFeatureToggleDirective } from './feature-toggle.directive';
 import { LgFeatureToggleModule } from './feature-toggle.module';
 import { notes } from './feature-toggle.notes';
 
-const stories = storiesOf('Directives', module);
 const options: LgFeatureToggleOptions = {
   // disable undefined feature e.g. Feature 4 in the following story
   defaultHide: true,
 };
 
-stories
-  .addParameters({
-    backgrounds: [{ name: 'default', value: '#0076d6', default: true }],
-  })
-  .add(
-    'Feature Toggle',
-    () => ({
-      moduleMetadata: {
-        imports: [
-          CanopyModule,
-          LgFeatureToggleModule.forRoot(
-            {
-              useFactory: () =>
-                of({
-                  firstFeature: true,
-                  secondFeature: false,
-                  thirdFeature: true,
-                  fourthFeature: false,
-                }),
-            },
-            options,
-          ),
-        ],
-      },
-      template: `
-  <lg-card *lgFeatureToggle="'firstFeature'"><lg-card-content>Feature 1 showing</lg-card-content></lg-card>
-  <lg-card *lgFeatureToggle="'secondFeature'"><lg-card-content>Feature 2 not showing</lg-card-content></lg-card>
-  <lg-card *lgFeatureToggle="'thirdFeature'"><lg-card-content>Feature 3 showing</lg-card-content></lg-card>
-  <lg-card *lgFeatureToggle="'fourthFeature'"><lg-card-content>Feature 4 not showing</lg-card-content></lg-card>
-  `,
+// This default export determines where your story goes in the story list
+export default {
+  title: 'Directives/FeatureToggle',
+  component: LgFeatureToggleDirective,
+  decorators: [
+    moduleMetadata({
+      imports: [
+        CanopyModule,
+        LgFeatureToggleModule.forRoot(
+          {
+            useFactory: () =>
+              of({
+                firstFeature: true,
+                secondFeature: false,
+                thirdFeature: true,
+                fourthFeature: false,
+              }),
+          },
+          options,
+        ),
+      ],
     }),
-    {
-      notes: { markdown: notes },
+  ],
+  parameters: {
+    docs: {
+      description: {
+        component: notes,
+      },
     },
-  );
+    controls: {
+      disable: true,
+    },
+    backgrounds: {
+      default: 'Super Blue',
+    },
+  },
+  argTypes: {
+    lgFeatureToggle: {
+      table: {
+        disable: true,
+      },
+    },
+    ngOnInit: {
+      table: {
+        disable: true,
+      },
+    },
+  },
+} as Meta;
+
+const template = `
+<lg-card *lgFeatureToggle="'firstFeature'"><lg-card-content>Feature 1 showing</lg-card-content></lg-card>
+<lg-card *lgFeatureToggle="'secondFeature'"><lg-card-content>Feature 2 not showing</lg-card-content></lg-card>
+<lg-card *lgFeatureToggle="'thirdFeature'"><lg-card-content>Feature 3 showing</lg-card-content></lg-card>
+<lg-card *lgFeatureToggle="'fourthFeature'"><lg-card-content>Feature 4 not showing</lg-card-content></lg-card>
+`;
+
+const featureToggleTemplate: Story<LgFeatureToggleDirective> = (
+  args: LgFeatureToggleDirective,
+) => ({
+  component: LgFeatureToggleDirective,
+  props: args,
+  template,
+});
+
+export const featureToggle = featureToggleTemplate.bind({});
+featureToggle.storyName = 'Feature Toggle';
+featureToggle.parameters = {
+  docs: {
+    source: {
+      code: template,
+    },
+  },
+};
