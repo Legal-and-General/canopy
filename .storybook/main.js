5995acba26b22021cb8287a728544eaea77c677f
Elena Garrone <8397116+elenagarrone@users.noreply.github.com>
2021-10-28 17:05:44 +0100

    feat(details): upgrade to CSF format

--- .storybook/main.js
+++ .storybook/main.js
@@ -6,6 +6,7 @@ module.exports = {
     '../projects/canopy/src/lib/alert/alert.stories.ts',
     '../projects/canopy/src/lib/feature-toggle/feature-toggle.stories.ts',
     '../projects/canopy/src/lib/focus/focus.stories.ts',
+    '../projects/canopy/src/lib/details/details.stories.ts',
   ],
   addons: [
     '@storybook/addon-links',
