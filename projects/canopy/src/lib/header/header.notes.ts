15498e211b0735459c369054737cd82465f21808
Elena Garrone <8397116+elenagarrone@users.noreply.github.com>
2021-11-02 11:04:46 +0000

    feat(header): upgrade to CSF format

--- projects/canopy/src/lib/header/header.notes.ts
+++ projects/canopy/src/lib/header/header.notes.ts
@@ -1,7 +1,4 @@
 export const notes = `
-# Header Component
-
-## Purpose
 Provides a generic header for display at the top of the page.
 The current implementation is brand agnostic but eventually the branding should be encapsulated into the component.
 The component uses an attribute selector which allows you to use the html [header](https://developer.mozilla.org/en-US/docs/Web/HTML/Element/header) element as the host.
@@ -17,24 +14,4 @@ Import the component in your application:
   imports: [LgHeaderModule],
 })
 ~~~
-
-and in your HTML:
-
-~~~html
-<header
-  lg-header
-  logo="/logo.svg"
-  logoAlt="Company Logo"
-  logoHref="http://company.com">
-</header>
-~~~
-
-## Inputs
-
-### LgHeaderComponent
-| Name | Description | Type | Default | Required |
-|------|-------------|:----:|:-----:|:-----:|
-| \`\`logo\`\` | A url link to the logo | string | null | Yes |
-| \`\`logoAlt\`\` | alt text to display alongside the logo | string | '2rem' | Yes |
-| \`\`logoHref\`\` | Url link if the logo is clickable | string | null | No |
 `;
