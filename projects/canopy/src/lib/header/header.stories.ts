d843da131c2eab284e8817004f1eb316e641e429
Elena Garrone <8397116+elenagarrone@users.noreply.github.com>
2021-11-02 11:04:46 +0000

    feat(header): upgrade to CSF format

--- projects/canopy/src/lib/header/header.stories.ts
+++ projects/canopy/src/lib/header/header.stories.ts
@@ -1,35 +1,67 @@
-import { text, withKnobs } from '@storybook/addon-knobs';
-import { moduleMetadata } from '@storybook/angular';
+import { Meta, moduleMetadata, Story } from '@storybook/angular';
 
-import { fullScreen } from '../../../../../.storybook/addons/full-screen';
 import { LgHeaderComponent } from '../header/header.component';
 import { notes } from './header.notes';
+import { LgHeaderModule } from './header.module';
 
 export default {
   title: 'Components/Header',
+  component: LgHeaderComponent,
+  decorators: [
+    moduleMetadata({
+      imports: [LgHeaderModule],
+    }),
+  ],
   parameters: {
-    decorators: [
-      fullScreen,
-      withKnobs,
-      moduleMetadata({
-        declarations: [LgHeaderComponent],
-      }),
-    ],
-    notes: {
-      markdown: notes,
+    docs: {
+      description: {
+        component: notes,
+      },
+    },
+  },
+  argTypes: {
+    logo: {
+      description: 'A url link to the logo.',
+    },
+    logoAlt: {
+      description: 'alt text to display alongside the logo.',
+      table: {
+        defaultValue: {
+          summary: '',
+        },
+      },
+    },
+    logoHref: {
+      description: 'Url link if the logo is clickable.',
+    },
+    class: {
+      table: {
+        disable: true,
+      },
+    },
+  },
+} as Meta;
+
+const template = `
+  <header lg-header [logo]="logo" [logoAlt]="logoAlt" [logoHref]="logoHref"></header>
+`;
+
+const detailsTemplate: Story<LgHeaderComponent> = (args: LgHeaderComponent) => ({
+  props: args,
+  template,
+});
+
+export const standardHeader = detailsTemplate.bind({});
+standardHeader.storyName = 'Standard';
+standardHeader.args = {
+  logo: 'legal-and-general-logo.svg',
+  logoAlt: 'Company name',
+  logoHref: 'https://storybook.js.org',
+};
+standardHeader.parameters = {
+  docs: {
+    source: {
+      code: template,
     },
   },
 };
-
-const groupId = 'header';
-
-export const standard = () => ({
-  template: `
-    <header lg-header [logo]="logo" [logoAlt]="logoAlt" [logoHref]="logoHref"></header>
-  `,
-  props: {
-    logo: text('logo', 'legal-and-general-logo.svg', groupId),
-    logoAlt: text('logoAlt', 'Company name', groupId),
-    logoHref: text('logoHref', 'https://storybook.js.org', groupId),
-  },
-});
