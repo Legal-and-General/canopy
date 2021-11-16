1f006c4bbf6f4cdfee285a129939ee3e557f407c
Elena Garrone <8397116+elenagarrone@users.noreply.github.com>
2021-11-16 17:19:10 +0000

    feat(separator): upgrade to CSF format

--- projects/canopy/src/lib/separator/separator.stories.ts
+++ projects/canopy/src/lib/separator/separator.stories.ts
@@ -1,30 +1,83 @@
-import { moduleMetadata } from '@storybook/angular';
-import { boolean, select, withKnobs } from '@storybook/addon-knobs';
+import { Meta, moduleMetadata, Story } from '@storybook/angular';
 
 import { LgSeparatorComponent } from '../separator/separator.component';
 import { notes } from './separator.notes';
+import { LgSeparatorModule } from './separator.module';
 
 export default {
   title: 'Components/Separator',
+  component: LgSeparatorComponent,
+  decorators: [
+    moduleMetadata({
+      imports: [LgSeparatorModule],
+    }),
+  ],
   parameters: {
-    decorators: [
-      withKnobs,
-      moduleMetadata({
-        declarations: [LgSeparatorComponent],
-      }),
-    ],
-    notes: {
-      markdown: notes,
+    docs: {
+      description: {
+        component: notes,
+      },
+    },
+  },
+  argTypes: {
+    variant: {
+      options: ['solid', 'dotted'],
+      description: 'The variant of separator.',
+      table: {
+        type: {
+          summary: ['solid', 'dotted'],
+        },
+        defaultValue: {
+          summary: 'solid',
+        },
+      },
+      control: {
+        type: 'select',
+      },
+    },
+    hasRole: {
+      description: 'If true, adds a role of ``separator`` to the component.',
+      table: {
+        type: {
+          summary: 'boolean',
+        },
+        defaultValue: {
+          summary: false,
+        },
+      },
+    },
+    class: {
+      table: {
+        disable: true,
+      },
+    },
+    hostElement: {
+      table: {
+        disable: true,
+      },
+    },
+  },
+} as Meta;
+
+const template = `
+  <lg-separator [variant]="variant" [hasRole]="hasRole"></lg-separator>
+`;
+
+const detailsTemplate: Story<LgSeparatorComponent> = (args: LgSeparatorComponent) => ({
+  props: args,
+  template,
+});
+
+export const standardSeparator = detailsTemplate.bind({});
+standardSeparator.storyName = 'Standard';
+standardSeparator.args = {
+  variant: 'solid',
+  hasRole: false,
+};
+standardSeparator.parameters = {
+  docs: {
+    source: {
+      code: template,
     },
   },
 };
-
-export const standard = () => ({
-  template: `
-    <lg-separator [variant]="variant" [hasRole]="hasRole"></lg-separator>
-  `,
-  props: {
-    variant: select('variant', ['solid', 'dotted'], 'solid', ''),
-    hasRole: boolean('hasRole', false),
-  },
-});
