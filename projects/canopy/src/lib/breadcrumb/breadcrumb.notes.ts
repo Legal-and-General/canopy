da564048c77283e1dc4c572de8771a05efaf21a5
Owen Evans <1943532+owensgit@users.noreply.github.com>
2021-12-01 15:58:48 +0000

    feat(breadcrumb): update story to CSF format

--- projects/canopy/src/lib/breadcrumb/breadcrumb.notes.ts
+++ projects/canopy/src/lib/breadcrumb/breadcrumb.notes.ts
@@ -1,13 +1,15 @@
 export const notes = `
-# Breadcrumb Component
+Displays the path of the user's journey from top-level through to child. 
 
+Note that there is different behavior on mobile vs desktop: 
 
-## Purpose
-Displays the path of the user's journey from top-level through to child. Note that there is different behavior on mobile vs desktop. Crumbs above the current level should be styled as links, and link to the previous level on desktop. It should display the root, current and previous levels of hierarchy.
-On mobile the crumb should take the user back to the previous level, but should be hidden at the top-level.
-
+* Desktop: The top-level (root), current and previous levels of hierarchy are displayed.
+* Mobile: Only one crumb is displayed, which takes the user back to the previous level.
 
 ## Usage
+
+Import the component in your module:
+
 ~~~js
 @NgModule({
   ...
@@ -15,42 +17,15 @@ On mobile the crumb should take the user back to the previous level, but should 
 })
 ~~~
 
-and in your HTML:
+### Displaying more than 3 items
 
-~~~html
-<lg-breadcrumb>
-  <lg-breadcrumb-item>
-    <a href="#">
-      <lg-icon [name]="'home'"></lg-icon>
-      Home
-    </a>
-  </lg-breadcrumb-item>
-  <lg-breadcrumb-item>
-    <a href="#">Products</a>
-  </lg-breadcrumb-item>
-  <lg-breadcrumb-item>
-    Pension
-  </lg-breadcrumb-item>
-</lg-breadcrumb>
-~~~
-
-## Configure Text Colour
-
-~~~html
-<lg-breadcrumb-item [variant]="'light'"></lg-breadcrumb-item>
-~~~
-
-## Adding an ellipsis
-
-If there are more than 3 levels of hierarchy, a collapsed breadcrumb should be used. This is achieved with the breadcrumb elipsis.
+If there are more than 3 levels of hierarchy, a collapsed breadcrumb should be used on desktop. This is achieved by using \`LgBreadcrumbItemEllipsisComponent\` (see full example below). 
 
 ~~~html
 <lg-breadcrumb-item-ellipsis></lg-breadcrumb-item-ellipsis>
 ~~~
 
-## Inputs
+*This is not automatically added and should be implemented in your app.*
 
-| Name | Description | Type | Default | Required |
-|------|-------------|:----:|:-----:|:-----:|
-| \`\`variant\`\` | The colour variants available: \`\`light\`\`, \`\`dark\`\` | string | dark | No |
+
 `;
