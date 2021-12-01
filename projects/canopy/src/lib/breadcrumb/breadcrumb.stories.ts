4e3a265f2a00c921f21687f1e5848558bd47ba86
Owen Evans <1943532+owensgit@users.noreply.github.com>
2021-12-01 15:58:48 +0000

    feat(breadcrumb): update story to CSF format

--- projects/canopy/src/lib/breadcrumb/breadcrumb.stories.ts
+++ projects/canopy/src/lib/breadcrumb/breadcrumb.stories.ts
@@ -1,127 +1,148 @@
-import { Component, OnInit, Input } from '@angular/core';
+import { Meta, moduleMetadata, Story } from '@storybook/angular';
 
-import { select, withKnobs } from '@storybook/addon-knobs';
-import { moduleMetadata } from '@storybook/angular';
-
+import { BreadcrumbVariant, LgBreadcrumbComponent } from '.';
 import { LgIconModule } from '../icon/icon.module';
-import { BreadcrumbVariant } from './breadcrumb-item/breadcrumb-item.interface';
 import { LgBreadcrumbModule } from './breadcrumb.module';
 import { notes } from './breadcrumb.notes';
 
-interface Breadcrumb {
-  text: string;
-}
-
-@Component({
-  selector: 'async-breadcrumb-story',
-  template: `
-    <lg-breadcrumb [variant]="variant">
-      <lg-breadcrumb-item *ngFor="let crumb of crumbs; index as i">
-        <a href="#" [attr.aria-current]="isCurrentAttribute(i)">
-          <lg-icon *ngIf="!i" [name]="'home'"></lg-icon>
-          {{ crumb.text }}
-        </a>
-      </lg-breadcrumb-item>
-    </lg-breadcrumb>
-  `,
-})
-export class AsyncBreadcrumbStoryComponent implements OnInit {
-  @Input() variant: BreadcrumbVariant;
-
-  crumbs: Array<Breadcrumb> = [];
-
-  currentPageIndex = this.crumbs.length - 1;
-
-  isCurrentAttribute(index: number) {
-    return this.currentPageIndex === index ? 'page' : null;
-  }
-
-  ngOnInit() {
-    const mockAsyncDelay = 500;
-
-    setTimeout(() => {
-      this.crumbs = [
-        { text: 'Home' },
-        { text: 'Home Insurance' },
-        { text: 'Change direct debit' },
-      ];
-    }, mockAsyncDelay);
-  }
-}
-
 export default {
   title: 'Components/Breadcrumb',
-  excludeStories: ['AsyncBreadcrumbStoryComponent'],
+  component: LgBreadcrumbComponent,
+  decorators: [
+    moduleMetadata({
+      imports: [LgIconModule, LgBreadcrumbModule],
+    }),
+  ],
   parameters: {
-    decorators: [
-      withKnobs,
-      moduleMetadata({
-        declarations: [AsyncBreadcrumbStoryComponent],
-        imports: [LgIconModule, LgBreadcrumbModule],
-      }),
-    ],
-    notes: {
-      markdown: notes,
+    docs: {
+      description: {
+        component: notes,
+      },
+    },
+  },
+  argTypes: {
+    variant: {
+      options: [BreadcrumbVariant.light, BreadcrumbVariant.dark],
+      description: 'The colour variant for use on light or dark backgrounds',
+      table: {
+        type: {
+          summary: [BreadcrumbVariant.light, BreadcrumbVariant.dark],
+        },
+        defaultValue: {
+          summary: BreadcrumbVariant.dark,
+        },
+      },
+      control: {
+        type: 'select',
+      },
+    },
+    attr: {
+      table: {
+        disable: true,
+      },
+    },
+    class: {
+      table: {
+        disable: true,
+      },
+    },
+    role: {
+      table: {
+        disable: true,
+      },
+    },
+    ngAfterContentChecked: {
+      table: {
+        disable: true,
+      },
+    },
+    crumbs: {
+      table: {
+        disable: true,
+      },
+    },
+    ellipsis: {
+      table: {
+        disable: true,
+      },
+    },
+  },
+} as Meta;
+
+const template = `
+<lg-breadcrumb [variant]="variant">
+  <lg-breadcrumb-item>
+    <a href="#">
+      <lg-icon [name]="'home'"></lg-icon>
+      Home
+    </a>
+  </lg-breadcrumb-item>
+  <lg-breadcrumb-item>
+    <a href="#">Products</a>
+  </lg-breadcrumb-item>
+  <lg-breadcrumb-item>
+    <a href="#">Pension Anniuty</a>
+  </lg-breadcrumb-item>
+</lg-breadcrumb>
+`;
+
+const breadcrumbTemplate: Story<LgBreadcrumbComponent> = (
+  args: LgBreadcrumbComponent,
+) => ({
+  props: args,
+  template,
+});
+
+export const threeItems = breadcrumbTemplate.bind({});
+threeItems.storyName = 'Three items';
+threeItems.args = {
+  variant: BreadcrumbVariant.dark,
+};
+threeItems.parameters = {
+  docs: {
+    source: {
+      code: template,
     },
   },
 };
 
-export const twoItems = () => ({
-  template: `
-    <lg-breadcrumb [variant]="variant">
-      <lg-breadcrumb-item>
-        <a href="#">
-          <lg-icon [name]="'home'"></lg-icon>
-          Home
-        </a>
-      </lg-breadcrumb-item>
-      <lg-breadcrumb-item>
-        <a href="#" aria-current="page">Your Pension Product</a>
-      </lg-breadcrumb-item>
-    </lg-breadcrumb>
-  `,
-  props: {
-    variant: select(
-      'Variant',
-      [BreadcrumbVariant.light, BreadcrumbVariant.dark],
-      BreadcrumbVariant.dark,
-    ),
-  },
+const ellipisTemplate = `
+<lg-breadcrumb [variant]="variant">
+  <lg-breadcrumb-item>
+    <a href="#">
+      <lg-icon [name]="'home'"></lg-icon>
+      Home
+    </a>
+  </lg-breadcrumb-item>
+
+  <!-- Epllipsis -->
+  <lg-breadcrumb-item-ellipsis></lg-breadcrumb-item-ellipsis>
+
+  <lg-breadcrumb-item>
+    <a href="#">Products</a>
+  </lg-breadcrumb-item>
+  <lg-breadcrumb-item>
+    <a href="#">Pension Anniuty</a>
+  </lg-breadcrumb-item>
+</lg-breadcrumb>
+`;
+
+const breadcrumbEllipsisTemplate: Story<LgBreadcrumbComponent> = (
+  args: LgBreadcrumbComponent,
+) => ({
+  props: args,
+  template: ellipisTemplate,
 });
 
-export const threeItems = () => ({
-  template: `
-    <lg-breadcrumb [variant]="variant">
-      <lg-breadcrumb-item>
-        <a href="#">
-          <lg-icon [name]="'home'"></lg-icon>
-          Home
-        </a>
-      </lg-breadcrumb-item>
-      <lg-breadcrumb-item>
-        <a href="#">Home Insurance</a>
-      </lg-breadcrumb-item>
-      <lg-breadcrumb-item>
-        <a href="#" aria-current="page">Change direct debit</a>
-      </lg-breadcrumb-item>
-    </lg-breadcrumb>
-  `,
-  props: {
-    variant: select(
-      'Variant',
-      [BreadcrumbVariant.light, BreadcrumbVariant.dark],
-      BreadcrumbVariant.dark,
-    ),
+export const ellipsis = breadcrumbEllipsisTemplate.bind({});
+ellipsis.storyName = 'More than three items';
+ellipsis.args = {
+  variant: BreadcrumbVariant.dark,
+};
+ellipsis.parameters = {
+  docs: {
+    source: {
+      code: ellipisTemplate,
+    },
   },
-});
-
-export const asynchronousLoading = () => ({
-  template: `<async-breadcrumb-story [variant]="variant"></async-breadcrumb-story>`,
-  props: {
-    variant: select(
-      'Variant',
-      [BreadcrumbVariant.light, BreadcrumbVariant.dark],
-      BreadcrumbVariant.dark,
-    ),
-  },
-});
+};
