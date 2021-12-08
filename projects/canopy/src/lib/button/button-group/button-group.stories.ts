0d7c6e893624c1d4c47da3cabed4d2d751fab564
Owen Evans <1943532+owensgit@users.noreply.github.com>
2021-12-08 14:45:40 +0000

    feat(buttons): update story to CSF format

---
+++ projects/canopy/src/lib/button/button-group/button-group.stories.ts
@@ -0,0 +1,58 @@
+import { Meta, moduleMetadata, Story } from '@storybook/angular';
+
+import { LgButtonGroupComponent, LgButtonModule } from 'projects/canopy/src/public-api';
+import { notes } from './button-group.notes';
+
+export default {
+  title: 'Components/Button Group',
+  component: LgButtonGroupComponent,
+  decorators: [
+    moduleMetadata({
+      imports: [LgButtonModule],
+    }),
+  ],
+  parameters: {
+    docs: {
+      description: {
+        component: notes,
+      },
+    },
+  },
+  argTypes: {
+    class: {
+      table: { disable: true },
+    },
+  },
+} as Meta;
+
+const template = `
+<lg-button-group>
+  <button
+    lg-button
+    variant="solid-primary"
+  >Button</button>
+  <a
+    lg-button
+    href="#"
+    variant="outline-primary"
+  >Link</a>
+</lg-button-group>
+`;
+
+const buttonGroupStory: Story<LgButtonGroupComponent> = (
+  args: LgButtonGroupComponent,
+) => ({
+  template,
+});
+
+export const standardButtonGroup = buttonGroupStory.bind({});
+standardButtonGroup.storyName = 'Standard';
+standardButtonGroup.parameters = {
+  controls: { hideNoControlsWarning: true },
+  docs: {
+    args: null,
+    source: {
+      code: template,
+    },
+  },
+};
