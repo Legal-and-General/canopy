495e7fe2440639cbfa0d5b155a2f345b997d20a3
Owen Evans <1943532+owensgit@users.noreply.github.com>
2021-12-14 11:16:30 +0000

    docs(data-point): update to CSF format for Storybook v6

--- projects/canopy/src/lib/data-point/data-point.stories.ts
+++ projects/canopy/src/lib/data-point/data-point.stories.ts
@@ -1,82 +1,111 @@
-import { select, withKnobs } from '@storybook/addon-knobs';
-import { moduleMetadata } from '@storybook/angular';
+import { moduleMetadata, Story } from '@storybook/angular';
 
-import { CanopyModule } from '../canopy.module';
+import { LgDataPointComponent, LgDataPointListComponent, LgDataPointModule } from '.';
+import { LgHeaderModule } from '../header';
 import { notes } from './data-point.notes';
 
 export default {
   title: 'Components/Data Point',
+  decorators: [
+    moduleMetadata({
+      imports: [LgDataPointModule, LgHeaderModule],
+    }),
+  ],
   parameters: {
-    decorators: [
-      withKnobs,
-      moduleMetadata({
-        imports: [CanopyModule],
-      }),
-    ],
-    notes: {
-      markdown: notes,
+    docs: {
+      description: {
+        component: notes,
+      },
+    },
+  },
+  argTypes: {
+    headingLevel: {
+      options: ['1', '2', '3', '4', '5', '6'],
+      description: 'The heading level set on the `<lg-data-point-label>` component',
+      table: {
+        type: {
+          summary: ['1', '2', '3', '4', '5', '6'],
+        },
+      },
+      control: {
+        type: 'select',
+      },
     },
   },
 };
 
-export const singleDataPoint = () => ({
-  template: `
-    <div lgContainer>
-      <div lgRow>
-        <div lgCol="12" lgColMd="8">
-          <lg-data-point>
-            <lg-data-point-label [headingLevel]="headingLevel">
-              Annual increase
-            </lg-data-point-label>
-            <lg-data-point-value>
-              Retail price index (RPI)
-            </lg-data-point-value>
-          </lg-data-point>
-        </div>
-      </div>
-    </div>
-  `,
-  props: {
-    headingLevel: select('headingLevel', [1, 2, 3, 4, 5, 6], 4),
-  },
+const singleTemplate = `
+<lg-data-point>
+  <lg-data-point-label [headingLevel]="headingLevel">
+    Annual increase
+  </lg-data-point-label>
+  <lg-data-point-value>
+    Retail price index (RPI)
+  </lg-data-point-value>
+</lg-data-point>
+`;
+
+const singleStory: Story<LgDataPointComponent> = (args: LgDataPointComponent) => ({
+  props: args,
+  template: singleTemplate,
 });
 
-export const dataPointList = () => ({
-  template: `
-    <div lgContainer>
-      <div lgRow>
-        <div lgCol="12" lgColMd="8">
-          <lg-data-point-list>
-            <lg-data-point>
-              <lg-data-point-label [headingLevel]="headingLevel">
-                Name on account
-              </lg-data-point-label>
-              <lg-data-point-value>
-                Joe Bloggs
-              </lg-data-point-value>
-            </lg-data-point>
-            <lg-data-point>
-              <lg-data-point-label [headingLevel]="headingLevel">
-                Account number
-              </lg-data-point-label>
-              <lg-data-point-value>
-                ***5678
-              </lg-data-point-value>
-            </lg-data-point>
-            <lg-data-point>
-              <lg-data-point-label [headingLevel]="headingLevel">
-                Bank sort code
-              </lg-data-point-label>
-              <lg-data-point-value>
-                00 - 00 - **
-              </lg-data-point-value>
-            </lg-data-point>
-          </lg-data-point-list>
-        </div>
-      </div>
-    </div>
-  `,
-  props: {
-    headingLevel: select('headingLevel', [1, 2, 3, 4, 5, 6], 4),
+export const singleDataPoint = singleStory.bind({});
+singleDataPoint.storyName = 'Single Data Point';
+singleDataPoint.args = {
+  headingLevel: 1,
+};
+singleDataPoint.parameters = {
+  docs: {
+    source: {
+      code: singleTemplate,
+    },
   },
+};
+
+const listTemplate = `
+<lg-data-point-list>
+  <lg-data-point>
+    <lg-data-point-label [headingLevel]="headingLevel">
+      Name on account
+    </lg-data-point-label>
+    <lg-data-point-value>
+      Joe Bloggs
+    </lg-data-point-value>
+  </lg-data-point>
+  <lg-data-point>
+    <lg-data-point-label [headingLevel]="headingLevel">
+      Account number
+    </lg-data-point-label>
+    <lg-data-point-value>
+      ***5678
+    </lg-data-point-value>
+  </lg-data-point>
+  <lg-data-point>
+    <lg-data-point-label [headingLevel]="headingLevel">
+      Bank sort code
+    </lg-data-point-label>
+    <lg-data-point-value>
+      00 - 00 - **
+    </lg-data-point-value>
+  </lg-data-point>
+</lg-data-point-list>
+`;
+
+const listStory: Story<LgDataPointListComponent> = (args: LgDataPointListComponent) => ({
+  props: args,
+  template: listTemplate,
 });
+
+export const dataPointsListStory = listStory.bind({});
+dataPointsListStory.storyName = 'Data point list';
+dataPointsListStory.args = {
+  headingLevel: 1,
+};
+dataPointsListStory.parameters = {
+  docs: {
+    source: {
+      code: listTemplate,
+    },
+  },
+};
