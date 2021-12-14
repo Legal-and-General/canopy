70a5690ce08bf7380313571ce7bc2f8bc12cbea5
Owen Evans <1943532+owensgit@users.noreply.github.com>
2021-12-14 11:16:30 +0000

    docs(data-point): update to CSF format for Storybook v6

--- projects/canopy/src/lib/data-point/data-point.notes.ts
+++ projects/canopy/src/lib/data-point/data-point.notes.ts
@@ -1,52 +1,11 @@
 export const notes = `
-# Data Point Component
+Display data organised by heading and value. A Data Point can be used on it's own, or grouped into a Data Point List where each one will be displayed horizontally and given a list-item role.
 
-## Purpose
-To display data organised by heading and value. A Data Point can be used on it's own, or grouped into a Data Point List where each one will be displayed horizontally and given a list-item role.
+Import the Data Point module in your module:
 
-## Usage
 ~~~js
 @NgModule({
   ...
   imports: [ ..., LgDataPointModule ],
 })
-~~~
-
-and in your HTML:
-
-~~~html
-<lg-data-point-list>
-  <lg-data-point>
-    <lg-data-point-label [headingLevel]="headingLevel">
-      Name on account
-    </lg-data-point-label>
-    <lg-data-point-value>
-      Joe Bloggs
-    </lg-data-point-value>
-  </lg-data-point>
-  <lg-data-point>
-    <lg-data-point-label [headingLevel]="headingLevel">
-      Account number
-    </lg-data-point-label>
-    <lg-data-point-value>
-      ***5678
-    </lg-data-point-value>
-  </lg-data-point>
-  <lg-data-point>
-    <lg-data-point-label [headingLevel]="headingLevel">
-      Bank sort code
-    </lg-data-point-label>
-    <lg-data-point-value>
-      00 - 00 - **
-    </lg-data-point-value>
-  </lg-data-point>
-</lg-data-point-list>
-~~~
-
-
-## Inputs
-
-| Name | Description | Type | Default | Required |
-|------|-------------|:----:|:-----:|:-----:|
-| headingLevel | Allows the heading level to be set | number | n/a | Yes |
-`;
+~~~`;
