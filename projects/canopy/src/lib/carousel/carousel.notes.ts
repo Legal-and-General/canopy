f3b71faa66564d16c66bd1fc2766178cd17fce8a
Elena Garrone <8397116+elenagarrone@users.noreply.github.com>
2021-12-14 15:32:32 +0000

    feat(carousel): update story to CSF format

--- projects/canopy/src/lib/carousel/carousel.notes.ts
+++ projects/canopy/src/lib/carousel/carousel.notes.ts
@@ -1,8 +1,4 @@
 export const notes = `
-# Carousel Component
-
-## Purpose
-
 Carousels allow customers to quickly navigate through grouped sections of content.
 
 ## Usage
@@ -12,33 +8,4 @@ Carousels allow customers to quickly navigate through grouped sections of conten
   imports: [ ..., LgCarouselModule ],
 })
 ~~~
-
-and in your HTML:
-
-~~~html
-<lg-carousel [description]="description" [headingLevel]="headingLevel" [loopMode]="true">
-  <lg-carousel-item>
-    Carousel item 1 content goes here
-  </lg-carousel-item>
-
-  <lg-carousel-item>
-    Carousel item 2 content goes here
-  </lg-carousel-item>
-
-  <lg-carousel-item>
-    Carousel item 3 content goes here
-  </lg-carousel-item>
-</lg-carousel>
-~~~
-
-## Carousel Inputs
-
-| Name                  | Description                                                                                              |  Type   |  Default  | Required |
-| --------------------- | -------------------------------------------------------------------------------------------------------- | :-----: | :-------: | :------: |
-| \`\`loopMode\`\`      | Allows the carousel to navigation to loop when the first or last item is active.                         | boolean |   false   |   No     |
-| \`\`slideDuration\`\` | Duration in milliseconds of the transition between slides.                                               | number  |   500     |   No     |
-| \`\`description\`\`   | Text used to describe the carousel content for screen readers. This is visually hidden.                  | string  | undefined |   Yes    |
-| \`\`headingLevel\`\`  | The heading level of the description: \`\`1\`\`, \`\`2\`\`, \`\`3\`\`, \`\`4\`\`, \`\`5\`\`, \`\`6\`\`   | number  | undefined |   Yes    |
-| \`\`autoPlay\`\`      | Enables auto play mode when set to true.                                                                 | boolean |   false   |   No     |
-| \`\`autoPlayDelay\`\` | Delay time in milliseconds to switch to next slide when autoPlay mode is set to true.                    | number  |   2000    |   No     |
 `;
