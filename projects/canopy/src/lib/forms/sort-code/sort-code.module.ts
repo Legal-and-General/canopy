d5027d4e0e9ab8e05c03b60e0bec3eaf30492503
Elena Garrone <8397116+elenagarrone@users.noreply.github.com>
2021-12-14 16:00:31 +0000

    feat(sort-code): remove old component
    BREAKING CHANGE: the sort component is now deprecated in favour of the new
    directive:
    
    ```
    <lg-input-field>
      Sort Code
      <lg-hint *ngIf="hint">Must be 6 digits long</lg-hint>
      <input lgInput lgSortCode formControlName="sortCode" />
    </lg-input-field>
    ```
    
    Resolves #613

--- projects/canopy/src/lib/forms/sort-code/sort-code.module.ts
+++ projects/canopy/src/lib/forms/sort-code/sort-code.module.ts
@@ -5,7 +5,6 @@ import { ReactiveFormsModule } from '@angular/forms';
 import { LgLabelModule } from '../label/label.module';
 import { LgHintModule } from '../hint/hint.module';
 import { LgInputModule } from '../input/input.module';
-import { LgSortCodeComponent } from './sort-code.component';
 import { LgMarginModule } from '../../spacing/margin';
 import { LgFocusModule } from '../../focus/focus.module';
 import { LgSortCodeDirective } from './sort-code.directive';
@@ -20,7 +19,7 @@ import { LgSortCodeDirective } from './sort-code.directive';
     LgMarginModule,
     LgFocusModule,
   ],
-  declarations: [LgSortCodeComponent, LgSortCodeDirective],
-  exports: [LgSortCodeComponent, LgSortCodeDirective],
+  declarations: [LgSortCodeDirective],
+  exports: [LgSortCodeDirective],
 })
 export class LgSortCodeModule {}
