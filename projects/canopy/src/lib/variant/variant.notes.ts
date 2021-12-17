598e5a92b0946b523b6a4e2b37df2a7f8a910064
Owen Evans <1943532+owensgit@users.noreply.github.com>
2021-12-17 16:30:54 +0000

    docs(variant): update to CSF format for Storybook v6

--- projects/canopy/src/lib/variant/variant.notes.ts
+++ projects/canopy/src/lib/variant/variant.notes.ts
@@ -1,15 +1,10 @@
 export const notes = `
 
-# Variant Directive
-
-## Purpose
 This directive allows you to add one of the five common colour variants to any Canopy component, such as \`\`info\`\` and \`\`success\`\`.
 
 They are already applied to existing Canopy components which use these variants out of the box, such as [Alert](/?path=/story/components-alert--standard), [Detail](?path=/story/components-details--standard) and [Form Validation](?path=/story/components-form-validation--standard), but this directive allows you to use them anywhere where required.
 
 This can be useful where you need the particular colour treatment (like the \`\`success\`\` variant for example), but your use-case does not fit one of the existing components.
-
-## Usage
 
 ~~~js
 @NgModule({
@@ -17,31 +12,4 @@ This can be useful where you need the particular colour treatment (like the \`\`
   imports: [LgVariantModule],
 })
 ~~~
-
-And in your HTML...
-
-~~~html
-<lg-card lgVariant="warning">
-  <lg-card-content>
-    I have the warning variant colours, yay!
-    <a href="#">Links are included</a>
-    <button lg-button variant="outline-primary">
-      Outline primary buttons too
-    </button>
-  </lg-card-content>
-</lg-card>
-~~~
-
-## Inputs
-
-The current available variants are:
-* \`\`generic\`\`
-* \`\`info\`\`
-* \`\`success\`\`
-* \`\`warning\`\`
-* \`\`error\`\`
-
-| Name | Description | Type | Default | Required |
-|------|-------------|:----:|:-----:|:-----:|
-| \`\`lgVariant\`\` | The name of variant desired | string | null | No |
 `;
