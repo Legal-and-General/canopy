e808dbb921e6c2a46d2ebf0f1c65aa54cccedc5c
Elena Garrone <8397116+elenagarrone@users.noreply.github.com>
2021-12-30 14:46:39 +0000

    docs(spinner): update story to CSF format

--- projects/canopy/src/lib/spinner/spinner.stories.ts
+++ projects/canopy/src/lib/spinner/spinner.stories.ts
@@ -1,31 +1,112 @@
-import { select, text, withKnobs } from '@storybook/addon-knobs';
-import { moduleMetadata } from '@storybook/angular';
+import { Meta, moduleMetadata, Story } from '@storybook/angular';
 
 import { LgSpinnerComponent } from './spinner.component';
 import { notes } from './spinner.notes';
+import { LgSpinnerModule } from './spinner.module';
 
+const variantTypes = ['dark', 'light', 'color', 'inherit'];
+const sizes = ['sm', 'md'];
+
+// This default export determines where your story goes in the story list
 export default {
   title: 'Components/Spinner',
+  component: LgSpinnerComponent,
+  decorators: [
+    moduleMetadata({
+      imports: [LgSpinnerModule],
+    }),
+  ],
   parameters: {
-    decorators: [
-      withKnobs,
-      moduleMetadata({
-        declarations: [LgSpinnerComponent],
-      }),
-    ],
-    notes: {
-      markdown: notes,
+    docs: {
+      description: {
+        component: notes,
+      },
+    },
+  },
+  argTypes: {
+    text: {
+      description: 'The text to show under the spinner.',
+    },
+    variant: {
+      options: variantTypes,
+      description:
+        "Applies colour treatment and ARIA role if applicable. If the ``variant`` is set to  ``'inherit'``, then it will inherit the font colour of its parent element. This is used on the loading button, where the spinner inherits the font colour of the button.",
+      table: {
+        type: {
+          summary: variantTypes,
+        },
+        defaultValue: {
+          summary: 'dark',
+        },
+      },
+      control: {
+        type: 'select',
+      },
+    },
+    size: {
+      options: sizes,
+      description: 'The size of the spinner.',
+      table: {
+        type: {
+          summary: sizes,
+        },
+        defaultValue: {
+          summary: 'md',
+        },
+      },
+      control: {
+        type: 'select',
+      },
+    },
+    class: {
+      table: {
+        disable: true,
+      },
+    },
+    readScreenReaderAlert: {
+      table: {
+        disable: true,
+      },
+    },
+    ngOnInit: {
+      table: {
+        disable: true,
+      },
+    },
+    ngOnDestroy: {
+      table: {
+        disable: true,
+      },
+    },
+  },
+} as Meta;
+
+const template = `
+<lg-spinner [size]="size" [variant]="variant" [text]="text ? text : null"></lg-spinner>
+`;
+
+const alertTemplate: Story<LgSpinnerComponent> = (args: LgSpinnerComponent) => ({
+  props: args,
+  template,
+});
+
+export const standardAlert = alertTemplate.bind({});
+standardAlert.storyName = 'Standard';
+standardAlert.args = {
+  text: 'Please wait while we load your data.',
+  variant: 'generic',
+  size: 'md',
+};
+standardAlert.parameters = {
+  docs: {
+    source: {
+      code: `
+# Display a spinner while loading
+<lg-spinner *ngIf="!loaded" [size]="size" [variant]="variant" [text]="text ? text : null"></lg-spinner>
+
+# Tell screen reader to read a message when loading is finished
+<p [lgSrAlertMessage]="loaded">Loading complete</p>
+      `,
     },
   },
 };
-
-export const standard = () => ({
-  template: `
-    <lg-spinner [size]="size" [variant]="variant" [text]="text ? text : null"></lg-spinner>
-  `,
-  props: {
-    variant: select('variant', ['dark', 'light', 'color', 'inherit'], 'dark'),
-    size: select('size', ['sm', 'md'], 'md'),
-    text: text('text', ''),
-  },
-});
