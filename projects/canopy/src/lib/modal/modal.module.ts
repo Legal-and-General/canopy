27a8078e99af564847578a806b44a27f4987e709
Elena Garrone <8397116+elenagarrone@users.noreply.github.com>
2022-01-10 18:19:17 +0000

    fix(modal): remove circular dependency

--- projects/canopy/src/lib/modal/modal.module.ts
+++ projects/canopy/src/lib/modal/modal.module.ts
@@ -2,15 +2,13 @@ import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
 import { A11yModule } from '@angular/cdk/a11y';
 
-import {
-  LgModalBodyComponent,
-  LgModalBodyTimerComponent,
-  LgModalComponent,
-  LgModalFooterComponent,
-  LgModalHeaderComponent,
-  LgModalService,
-  LgModalTriggerDirective,
-} from './';
+import { LgModalBodyComponent } from './modal-body/modal-body.component';
+import { LgModalBodyTimerComponent } from './modal-body-timer/modal-body-timer.component';
+import { LgModalComponent } from './modal.component';
+import { LgModalFooterComponent } from './modal-footer/modal-footer.component';
+import { LgModalService } from './modal.service';
+import { LgModalTriggerDirective } from './modal-trigger/modal-trigger.directive';
+import { LgModalHeaderComponent } from './modal-header/modal-header.component';
 import { LgSeparatorModule } from '../separator';
 import { lgIconClose, LgIconModule, LgIconRegistry } from '../icon';
 import { LgFocusModule } from '../focus';
