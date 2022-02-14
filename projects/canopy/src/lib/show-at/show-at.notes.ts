c05134cee89cf2f34c83e749e9ae6c63a2847673
Elena Garrone <8397116+elenagarrone@users.noreply.github.com>
2022-02-14 10:43:29 +0000

    docs(show-at): update story to CSF format

--- projects/canopy/src/lib/show-at/show-at.notes.ts
+++ projects/canopy/src/lib/show-at/show-at.notes.ts
@@ -1,7 +1,4 @@
 export const notes = `
-# Show At Directive
-
-## Purpose
 This utility directive allows for mobile-first media queries to be added to components, so they can be shown at the desired breakpoint, without the need to write additional CSS.
 
 For example, you may need to show a component only when the viewport is wide enough, e.g. desktop.
@@ -20,22 +17,4 @@ Import the module in your application:
   imports: [LgShowAtModule],
 })
 ~~~
-
-And in your HTML...
-
-~~~html
-<lg-card lgShowAt="md">
-  <lg-card-content>
-    I get dispalayed at the "md" breakpoint
-  </lg-card-content>
-</lg-card>
-~~~
-
-## Inputs
-
-The current available breakpoints are 'sm', md', 'lg', 'xl', and 'xxl'.
-
-| Name | Description | Type | Default | Required |
-|------|-------------|:----:|:-----:|:-----:|
-| \`\`lgShowAt\`\` | The name of the breakpoint applied | string | null | No |
 `;
