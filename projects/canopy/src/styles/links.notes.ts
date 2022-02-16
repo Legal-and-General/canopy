eddeefd49f26c1fa24389b7812bc89c0a205228e
Paul Bennett <4497410+paul-request@users.noreply.github.com>
2022-02-16 13:44:27 +0000

    docs(links): update to CSF format

--- projects/canopy/src/styles/links.notes.ts
+++ projects/canopy/src/styles/links.notes.ts
@@ -1,6 +1,4 @@
 export const notes = `
-# Links
-
 The links styles are applied automatically when adding the global Canopy css.
 
 For custom components it's possible to override the style of a link by using the \`lg-unstyled-link\`.
