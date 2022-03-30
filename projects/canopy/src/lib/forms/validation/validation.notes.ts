722e52944771d8df9da1646271adb16224d6e391
Elena Garrone <8397116+elenagarrone@users.noreply.github.com>
2022-03-30 18:44:37 +0100

    docs(validation): update to CSF format

--- projects/canopy/src/lib/forms/validation/validation.notes.ts
+++ projects/canopy/src/lib/forms/validation/validation.notes.ts
@@ -1,8 +1,4 @@
 export const notes = `
-# Error Component
-
-
-## Purpose
 The validation component is used to provide contextual information for a form input field. The most common use case is to display validation errors however it can also be used to show additional information.
 
 
@@ -13,19 +9,4 @@ The validation component is used to provide contextual information for a form in
   imports: [ ..., LgValidationModule ],
 })
 ~~~
-
-and in your HTML:
-
-~~~html
-<lg-validation>Enter a valid postcode</lg-validation>
-
-<lg-validation variant="success">Username is available</lg-validation>
-~~~
-
-## Inputs
-
-| Name | Description | Type | Default | Required |
-|------|-------------|:----:|:-----:|:-----:|
-| \`\`variant\`\` | The variant of the validation: \`\`info\`\`, \`\`warning\`\`, \`\`error\`\`, \`\`success\`\` | string | 'error' | No |
-| \`\`showIcon\`\` | Whether the icon should display | boolean | true | No |
 `;
