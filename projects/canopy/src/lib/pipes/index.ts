fc9740b268bc57605b2062a23d25f93898e2929f
Stefan Timosenco <12414812+st3fun1@users.noreply.github.com>
2022-03-30 18:24:39 +0300

    fix(pipes): add module exports for existing pipes

--- projects/canopy/src/lib/pipes/index.ts
+++ projects/canopy/src/lib/pipes/index.ts
@@ -1,2 +1,4 @@
+export * from './camel-case/camel-case.module';
 export * from './camel-case/camel-case.pipe';
+export * from './kebab-case/kebab-case.module';
 export * from './kebab-case/kebab-case.pipe';
