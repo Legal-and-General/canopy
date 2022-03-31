ca8601e4d031451e3fa2322523539b1048ab1389
Elena Garrone <8397116+elenagarrone@users.noreply.github.com>
2022-03-31 14:27:48 +0100

    docs(tabs): update to CSF format

--- projects/canopy/src/lib/tabs/tab-nav-bar.stories.ts
+++ projects/canopy/src/lib/tabs/tab-nav-bar.stories.ts
@@ -1,42 +1,134 @@
-import { object, withKnobs } from '@storybook/addon-knobs';
-import { moduleMetadata } from '@storybook/angular';
+import { moduleMetadata, Story } from '@storybook/angular';
 
 import { LgMarginModule } from '../spacing';
 import { LgTabsModule } from './tabs.module';
 import { tabbedNavNotes } from './tabs.notes';
+import { LgTabsComponent } from './tabs.component';
 
 export default {
-  title: 'Components/Tabs/Tabbed Navigation',
+  title: 'Components/Tabs/Tabbed navigation',
+  component: LgTabsComponent,
+  decorators: [
+    moduleMetadata({
+      imports: [LgTabsModule, LgMarginModule],
+    }),
+  ],
   parameters: {
-    decorators: [
-      withKnobs,
-      moduleMetadata({
-        imports: [LgTabsModule, LgMarginModule],
-      }),
-    ],
-    notes: {
-      markdown: tabbedNavNotes,
+    docs: {
+      description: {
+        component: tabbedNavNotes,
+      },
+    },
+  },
+  argTypes: {
+    onClick: {
+      action: 'Tab nav link click',
+      table: {
+        disable: true,
+      },
+    },
+    tabEvent: {
+      table: {
+        disable: true,
+      },
+    },
+    label: {
+      table: {
+        disable: true,
+      },
+    },
+    class: {
+      table: {
+        disable: true,
+      },
+    },
+    selectedTabNavIndex: {
+      table: {
+        disable: true,
+      },
+    },
+    id: {
+      table: {
+        disable: true,
+      },
+    },
+    isKeyboardEvent: {
+      table: {
+        disable: true,
+      },
+    },
+    selectedIndex: {
+      table: {
+        disable: true,
+      },
+    },
+    blur: {
+      table: {
+        disable: true,
+      },
+    },
+    keyboardNavigation: {
+      table: {
+        disable: true,
+      },
+    },
+    navigateToTab: {
+      table: {
+        disable: true,
+      },
+    },
+    ngAfterContentInit: {
+      table: {
+        disable: true,
+      },
+    },
+    ngOnDestroy: {
+      table: {
+        disable: true,
+      },
+    },
+    setTabs: {
+      table: {
+        disable: true,
+      },
+    },
+    tabQueryList: {
+      table: {
+        disable: true,
+      },
     },
   },
 };
 
-export const standard = () => ({
-  template: `
-  <lg-tab-nav-bar label="Tabbed navigation demo">
-    <a *ngFor="let tab of tabs; index as i" lgTabNavBarLink href="" (click)="onClick($event)" id="tabbed-nav-{{i}}" [isActive]="i === selectedTabNavIndex">{{tab}}</a>
-  </lg-tab-nav-bar>
+function getTemplate(preventDefault?: boolean) {
+  return `
+<lg-tab-nav-bar label="Tabbed navigation demo">
+  <a *ngFor="let tab of tabs; index as i" lgTabNavBarLink href="" (click)="${
+    preventDefault ? '$event.preventDefault();' : ''
+  }onClick($event)" id="tabbed-nav-{{i}}" [isActive]="i === selectedTabNavIndex">{{tab}}</a>
+</lg-tab-nav-bar>
 
-  <lg-tab-nav-content [selectedTabId]="'tabbed-nav-' + selectedTabNavIndex">
-    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi posuere nec leo nec hendrerit. Pellentesque eu lacinia tortor. Donec urna felis, dictum et faucibus et, interdum ut urna. Nam eleifend eleifend mi vel blandit. Morbi rutrum a odio in pharetra. Quisque vitae lacus efficitur, elementum mauris in, porta nisl. Praesent porttitor accumsan ante, sed efficitur nunc placerat in. Etiam non lorem leo. Aenean magna lacus, iaculis at velit non, congue commodo dui. Pellentesque tempus elementum tempor.</p>
-  </lg-tab-nav-content>
-  `,
-  props: {
-    tabs: object('Tabs', getDefaultTabsContent()),
-    selectedTabNavIndex: 1,
-    onClick: (event: MouseEvent) => event.preventDefault(),
-  },
+<lg-tab-nav-content [selectedTabId]="'tabbed-nav-' + selectedTabNavIndex">
+  <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi posuere nec leo nec hendrerit. Pellentesque eu lacinia tortor. Donec urna felis, dictum et faucibus et, interdum ut urna. Nam eleifend eleifend mi vel blandit. Morbi rutrum a odio in pharetra. Quisque vitae lacus efficitur, elementum mauris in, porta nisl. Praesent porttitor accumsan ante, sed efficitur nunc placerat in. Etiam non lorem leo. Aenean magna lacus, iaculis at velit non, congue commodo dui. Pellentesque tempus elementum tempor.</p>
+</lg-tab-nav-content>
+  `;
+}
+
+const tabNavBarStory: Story<LgTabsModule> = (args: LgTabsModule) => ({
+  props: args,
+  template: getTemplate(true),
 });
 
-function getDefaultTabsContent(): Array<string> {
-  return ['Tab 1', 'Tab 2', 'Tab 3', 'Tab 4'];
-}
+export const tabNavBar = tabNavBarStory.bind({});
+tabNavBar.storyName = 'Tabbed navigation';
+tabNavBar.args = {
+  tabs: ['Tab 1', 'Tab 2', 'Tab 3', 'Tab 4'],
+  selectedTabNavIndex: 1,
+};
+tabNavBar.parameters = {
+  docs: {
+    source: {
+      code: getTemplate(),
+    },
+  },
+};
