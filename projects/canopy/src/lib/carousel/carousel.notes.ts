03cd180953500f3ab8506adae97ee52a827fff83
Elena Garrone <8397116+elenagarrone@users.noreply.github.com>
2022-04-05 16:34:23 +0100

    docs(carousel): add inputs to notes

--- projects/canopy/src/lib/carousel/carousel.notes.ts
+++ projects/canopy/src/lib/carousel/carousel.notes.ts
@@ -9,5 +9,16 @@ Carousels allow customers to quickly navigate through grouped sections of conten
 })
 ~~~
 
-PlayCarousel and PauseCarousel methods can be exposed by targeting the Carousel with ViewChild decorators.
+## Inputs
+
+| Name                  | Description                                                                                              |  Type   |  Default  | Required |
+| --------------------- | -------------------------------------------------------------------------------------------------------- | :-----: | :-------: | :------: |
+| \`\`loopMode\`\`      | Allows the carousel to navigation to loop when the first or last item is active.                         | boolean |   false   |   No     |
+| \`\`slideDuration\`\` | Duration in milliseconds of the transition between slides.                                               | number  |   500     |   No     |
+| \`\`description\`\`   | Text used to describe the carousel content for screen readers. This is visually hidden.                  | string  | undefined |   Yes    |
+| \`\`headingLevel\`\`  | The heading level of the description: \`\`1\`\`, \`\`2\`\`, \`\`3\`\`, \`\`4\`\`, \`\`5\`\`, \`\`6\`\`   | number  | undefined |   Yes    |
+| \`\`autoPlay\`\`      | Enables auto play mode when set to true.                                                                 | boolean |   false   |   No     |
+| \`\`autoPlayDelay\`\` | Delay time in milliseconds to switch to next slide when autoPlay mode is set to true.                    | number  |   2000    |   No     |
+
+\`PlayCarousel\` and \`PauseCarousel\` methods can be exposed by targeting the Carousel with \`ViewChild\` decorators.
 `;
