971d7218d58804a8eb47c80d896ef67e47b241a2
Elena Garrone <8397116+elenagarrone@users.noreply.github.com>
2022-04-05 17:31:09 +0100

    docs(radio): remove unnecessary argTypes

--- projects/canopy/src/lib/forms/radio/radio.stories.ts
+++ projects/canopy/src/lib/forms/radio/radio.stories.ts
@@ -72,38 +72,18 @@ export default {
   },
   argTypes: {
     id: {
-      description: 'HTML ID attribute, auto generated if not provided.',
-      control: false,
       table: {
-        defaultValue: {
-          summary: 'lg-radio-group-id-${nextUniqueId++}',
-        },
-        type: {
-          summary: 'string',
-        },
+        disable: true,
       },
     },
     name: {
-      description:
-        'Set the name value for all inputs in the group, auto-generated if not provided.',
-      control: false,
       table: {
-        defaultValue: {
-          summary: 'lg-radio-group-${nextUniqueId++}',
-        },
-        type: {
-          summary: 'string',
-        },
+        disable: true,
       },
     },
     value: {
-      description:
-        'Set the default checked radio button, must match the value of the radio button.',
-      control: false,
       table: {
-        type: {
-          summary: 'string',
-        },
+        disable: true,
       },
     },
     focus: {
@@ -140,13 +120,8 @@ export default {
       },
     },
     ariaDescribedBy: {
-      description:
-        'HTML ID for the corresponding element that describes the filters, if not provided it will use the hint field where appropriate.',
-      control: false,
       table: {
-        type: {
-          summary: 'string',
-        },
+        disable: true,
       },
     },
     radioChange: {
