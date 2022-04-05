83553de7abc7dd4e2dd10ad1676843091d55b840
Elena Garrone <8397116+elenagarrone@users.noreply.github.com>
2022-04-05 16:02:56 +0100

    docs(breadcrumb): update notes

--- projects/canopy/src/lib/breadcrumb/breadcrumb.notes.ts
+++ projects/canopy/src/lib/breadcrumb/breadcrumb.notes.ts
@@ -1,7 +1,7 @@
 export const notes = `
-Displays the path of the user's journey from top-level through to child. 
+Displays the path of the user's journey from top-level through to child.
 
-Note that there is different behavior on mobile vs desktop: 
+Note that there is different behavior on mobile vs desktop:
 
 * Desktop: The top-level (root), current and previous levels of hierarchy are displayed.
 * Mobile: Only one crumb is displayed, which takes the user back to the previous level.
@@ -13,19 +13,24 @@ Import the component in your module:
 ~~~js
 @NgModule({
   ...
-  declarations: [ ..., LgBreadcrumbModule ],
+  imports: [ ..., LgBreadcrumbModule ],
 })
 ~~~
 
 ### Displaying more than 3 items
 
-If there are more than 3 levels of hierarchy, a collapsed breadcrumb should be used on desktop. This is achieved by using \`LgBreadcrumbItemEllipsisComponent\` (see full example below). 
+If there are more than 3 levels of hierarchy, a collapsed breadcrumb should be used on desktop. This is achieved by using \`LgBreadcrumbItemEllipsisComponent\` (see full example below).
 
 ~~~html
+
 <lg-breadcrumb-item-ellipsis></lg-breadcrumb-item-ellipsis>
+
 ~~~
 
 *This is not automatically added and should be implemented in your app.*
 
-
+## Breadcrumb inputs
+| Name | Description | Type | Default | Required |
+|------|-------------|:----:|:-----:|:-----:|
+| \`\`variant\`\` | The colour variants available: \`\`light\`\`, \`\`dark\`\` | string | dark | No |
 `;
