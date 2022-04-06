eb6ae1c9630ead4941c91f499f2165f267c4d308
Elena Garrone <8397116+elenagarrone@users.noreply.github.com>
2022-04-06 09:15:21 +0100

    docs(separator): add inputs to notes

--- projects/canopy/src/lib/separator/separator.notes.ts
+++ projects/canopy/src/lib/separator/separator.notes.ts
@@ -6,7 +6,13 @@ Separators are used to separate individual component or group of components.
 ~~~js
 @NgModule({
     ...
-    declarations: [ ..., LgSeparatorModule ],
+    imports: [ ..., LgSeparatorModule ],
 })
 ~~~
+
+## Inputs
+| Name | Description | Type | Default | Required |
+|------|-------------|:----:|:-----:|:-----:|
+| \`\`variant\`\` | The variant of separator: \`\`solid\`\`, \`\`dotted\`\` | SeparatorVariant | 'solid' | No |
+| \`\`hasRole\`\` | If true, adds a role of \`\`separator\`\` to the component | boolean | false | No |
 `;
