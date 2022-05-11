d7aee88d6456baad53c353f7a501a3bc33da889e
Elena Garrone <8397116+elenagarrone@users.noreply.github.com>
2022-05-11 17:50:33 +0100

    docs(gh-pages): fix issue with catching pr data error

--- .github/workflow-scripts/gh-pages/get-pr-data.js
+++ .github/workflow-scripts/gh-pages/get-pr-data.js
@@ -3,18 +3,17 @@ module.exports = async ({
   github,
   context: { repo: { repo, owner } },
 }) => {
-  pullNumber = parseInt(pullNumber.replace('#', '').trim());
+  try {
+    pullNumber = parseInt(pullNumber.replace('#', '').trim());
 
-  const { data: pullsList } = await github.rest.pulls.get({
-    owner,
-    repo,
-    pull_number: pullNumber,
-  });
+    const { data: { head: { sha, ref: branch } } } = await github.rest.pulls.get({
+      owner,
+      repo,
+      pull_number: pullNumber,
+    });
 
-  if (!pullsList.length) {
-    throw `🚫 Error: PR #${pullNumber} was not found`
+    return { sha, branch, pullNumber };
+  } catch {
+    throw `🚫 Error: PR #${pullNumber} was not found`;
   }
-
-  const { head: { sha, ref: branch } } = pullsList;
-  return { sha, branch, pullNumber };
 }
