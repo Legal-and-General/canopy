f722824eaf68263bd172dfeb0fa0cd1e711dc2d5
Elena Garrone <8397116+elenagarrone@users.noreply.github.com>
2022-05-11 16:26:40 +0100

    fix: comment

--- .github/workflow-scripts/gh-pages/add-branch-deployed-comment.js
+++ .github/workflow-scripts/gh-pages/add-branch-deployed-comment.js
@@ -5,13 +5,13 @@ module.exports = async ({
   context: { repo: { repo, owner } },
 }) => {
   try {
-    const { data: labels } = await github.rest.issues.listLabelsOnIssue({
+    const labels = await github.rest.issues.listLabelsOnIssue({
       owner,
       repo,
       issue_number: pullNumber,
     });
 
-    if (!labels?.length || !labels.find(({ name }) => name === 'deployed')) {
+    if (!labels.length || !labels.find(({ name }) => name === 'deployed')) {
       await github.rest.issues.createComment({
         owner,
         repo,
@@ -19,6 +19,7 @@ module.exports = async ({
         body: `### :rocket: Branch deployed\n*Note that the deployment might take ~1 minute before being available.*\n\nThe **branch URL** is:\nhttps://legal-and-general.github.io/canopy/sb-${branch}`
       });
     }
+
     return labels;
   } catch (e) {
     console.info(e);
