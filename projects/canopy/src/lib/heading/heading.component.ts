527f1e5e087cc678f616ff99bc585de1aaceb00f
Elena Garrone <8397116+elenagarrone@users.noreply.github.com>
2022-06-07 16:15:36 +0100

    fix(heading): update heading to allow spacing directives
    The lg-heading component had a bug that wasn't allowing the padding
    and margin directives to work. This fixes the issue.
    
    BREAKING CHANGE: the instances of the lg-heading component
    used in your projects should be reviewed. If either a padding or
    margin directive was used, know that it wasn't previously
    working so it might now look different.

--- projects/canopy/src/lib/heading/heading.component.ts
+++ projects/canopy/src/lib/heading/heading.component.ts
@@ -5,7 +5,11 @@ import type { HeadingLevel } from './heading.interface';
 @Component({
   selector: 'lg-heading',
   templateUrl: './heading.component.html',
+  styleUrls: [ 'heading.component.scss' ],
   encapsulation: ViewEncapsulation.None,
+  host: {
+    class: 'lg-heading',
+  },
 })
 export class LgHeadingComponent {
   @Input() level: HeadingLevel;
