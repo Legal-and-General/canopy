3357635019258c700501d87ffc18fb21fe4cbec8
Elena Garrone <8397116+elenagarrone@users.noreply.github.com>
2022-07-05 18:01:18 +0100

    feat(header): update component API
    The component API has been changed for consistency with all the other components.
    
    BREAKING CHANGE: the logos are now components on their own instead
    of being injected as inputs in the lg-header component. Please
    update your application code to be:
    ```
      <header lg-header>
        <lg-header-logo [src]="logoSrc" [alt]="logoAlt" [href]="logoHref"></lg-header-logo>
    
        <!--  Additional code can be inserted here  -->
      </header>
    ```
    and for co-branded scenarios:
    ```
      <header lg-header>
        <lg-header-logo [src]="logoSrc" [alt]="logoAlt" [href]="logoHref"></lg-header-logo>
        <lg-header-logo [src]="secondLogoSrc" [alt]="secondLogoAlt" [href]="secondLogoHref"></lg-header-logo>
    
        <!--  Additional code can be inserted here  -->
      </header>
    ```

--- projects/canopy/src/lib/header/header.module.ts
+++ projects/canopy/src/lib/header/header.module.ts
@@ -3,11 +3,12 @@ import { NgModule } from '@angular/core';
 
 import { LgGridModule } from '../grid';
 
+import { LgHeaderLogoComponent } from './header-logo/header-logo.component';
 import { LgHeaderComponent } from './header.component';
 
 @NgModule({
-  declarations: [ LgHeaderComponent ],
-  exports: [ LgHeaderComponent ],
+  declarations: [ LgHeaderComponent, LgHeaderLogoComponent ],
+  exports: [ LgHeaderComponent, LgHeaderLogoComponent ],
   entryComponents: [ LgHeaderComponent ],
   imports: [ CommonModule, LgGridModule ],
 })
