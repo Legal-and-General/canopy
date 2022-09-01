dc0d85e6c196185de242fc791d5fdf9851bd509e
Elena Garrone <8397116+elenagarrone@users.noreply.github.com>
2022-09-01 17:38:45 +0100

    build(storybook): fix bug with canvas and docs tab
    Fix a bug with Storybook where clicking on the 'Guide' and then on one of the 'Examples'
    would show the Docs tab even though it's disabled.

--- .storybook/manager.js
+++ .storybook/manager.js
@@ -4,3 +4,18 @@ import canopyTheme from './canopy-theme';
 addons.setConfig({
   theme: canopyTheme,
 });
+
+/*
+* The code below fixes a bug with Storybook where clicking on the 'Guide' and then on one of the 'Examples'
+* would show the Docs tab even though it's disabled.
+* This bug might be fixed when the following issue is resolved: https://github.com/storybookjs/storybook/issues/18225
+*/
+document.addEventListener('click', ({ target }) => {
+  if (target.classList.contains('sidebar-item')) {
+    for (const canvasButton of document.querySelectorAll('button[type="button"]')) {
+      if (canvasButton.textContent.includes('Canvas')) {
+        canvasButton.click();
+      }
+    }
+  }
+});
