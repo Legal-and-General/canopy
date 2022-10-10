5b0bed0be9e86e2c3d89e3cf3229e09ec8d5b0b6
Walter Reis <9714275+Xinchro@users.noreply.github.com>
2022-10-10 09:18:01 +0100

    docs(separator): update separator docs and structure

---
+++ projects/canopy/src/lib/separator/docs/separator.stories.ts
@@ -0,0 +1,81 @@
+import { Meta, moduleMetadata, Story } from '@storybook/angular';
+
+import { LgSeparatorComponent } from '../separator.component';
+import { LgSeparatorModule } from '../separator.module';
+
+export default {
+  title: 'Components/Separator/Examples',
+  component: LgSeparatorComponent,
+  decorators: [
+    moduleMetadata({
+      imports: [ LgSeparatorModule ],
+    }),
+  ],
+  parameters: {
+    viewMode: 'story',
+    previewTabs: { 'storybook/docs/panel': { hidden: true } },
+  },
+  argTypes: {
+    variant: {
+      options: [ 'solid', 'dotted' ],
+      description: 'The variant of separator.',
+      table: {
+        type: {
+          summary: [ 'solid', 'dotted' ],
+        },
+        defaultValue: {
+          summary: 'solid',
+        },
+      },
+      control: {
+        type: 'select',
+      },
+    },
+    hasRole: {
+      description: 'If true, adds a role of ``separator`` to the component.',
+      table: {
+        type: {
+          summary: 'boolean',
+        },
+        defaultValue: {
+          summary: false,
+        },
+      },
+    },
+    class: {
+      table: {
+        disable: true,
+      },
+    },
+    hostElement: {
+      table: {
+        disable: true,
+      },
+    },
+  },
+} as Meta;
+
+const template = `
+  <lg-separator [variant]="variant" [hasRole]="hasRole"></lg-separator>
+`;
+
+const detailsTemplate: Story<LgSeparatorComponent> = (args: LgSeparatorComponent) => ({
+  props: args,
+  template,
+});
+
+export const standardSeparator = detailsTemplate.bind({});
+standardSeparator.storyName = 'Separator';
+
+standardSeparator.args = {
+  variant: 'solid',
+  hasRole: false,
+};
+
+standardSeparator.parameters = {
+  docs: {
+    source: {
+      code: template,
+    },
+  },
+};
