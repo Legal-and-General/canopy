87ca81a85707b0cf3aa23db29ece86e703c2337b
Walter Reis <9714275+Xinchro@users.noreply.github.com>
2022-10-11 11:03:11 +0100

    docs: fixed apostrophes in various places

--- projects/canopy/src/lib/icon/icon.notes.ts
+++ projects/canopy/src/lib/icon/icon.notes.ts
@@ -44,7 +44,7 @@ All icons have height and width equal to 1em. This means the icons will be as bi
 
 *By default the icons have the \`\`fill\`\` css property set to \`\`currentColor\`\`.*
 
-> \`\`currentColor\`\` acts like a variable for the current value of the \`\`color\`\` property on the element. And the Cascading part of CSS is still effective, so if there’s no defined \`\`color\`\` property on an element, the cascade will determine the value of \`\`currentColor\`\`.
+> \`\`currentColor\`\` acts like a variable for the current value of the \`\`color\`\` property on the element. And the Cascading part of CSS is still effective, so if there\'s no defined \`\`color\`\` property on an element, the cascade will determine the value of \`\`currentColor\`\`.
 >
 > *Understanding the currentColor Keyword in CSS - https://www.digitalocean.com/community/tutorials/css-currentcolor*
 
