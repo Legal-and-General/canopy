4baacb97b9d476347186471c524c3190d548611a
Elena Garrone <8397116+elenagarrone@users.noreply.github.com>
2022-10-28 12:15:11 +0100

    docs(link): update docs and structure and move into components

--- projects/canopy/src/styles/links.notes.ts
+++
@@ -1,5 +0,0 @@
-export const notes = `
-The links styles are applied automatically when adding the global Canopy css.
-
-For custom components it's possible to override the style of a link by using the \`lg-unstyled-link\`.
-`;
