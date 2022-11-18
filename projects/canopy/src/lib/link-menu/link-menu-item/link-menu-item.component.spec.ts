cb9853116fe3bc46a88d089aae8c3d4470fb8541
Mark Blandford <8008207+markblandford@users.noreply.github.com>
2022-11-18 18:03:19 +0000

    fix(link-menu-item): a11y text is set based on parent anchor target
    * Now references the parent to determine what the target is and if it is "_blank" to then correctly
    add the 'opens in a new tab' text.

--- projects/canopy/src/lib/link-menu/link-menu-item/link-menu-item.component.spec.ts
+++ projects/canopy/src/lib/link-menu/link-menu-item/link-menu-item.component.spec.ts
@@ -17,6 +17,7 @@ import { LgLinkMenuItemComponent } from './link-menu-item.component';
 })
 class TestComponent {
   @Input() internal = true;
+  @Input() target: string = undefined;
 }
 
 describe('LgLinkMenuItemComponent', () => {
@@ -37,53 +38,107 @@ describe('LgLinkMenuItemComponent', () => {
     }).compileComponents();
   });
 
-  beforeEach(() => {
-    fixture = TestBed.createComponent(TestComponent);
-    component = fixture.componentInstance;
-    component.internal = true;
-    fixture.detectChanges();
+  describe('component tests', () => {
+    beforeEach(() => {
+      fixture = TestBed.createComponent(TestComponent);
+      component = fixture.componentInstance;
+      component.internal = true;
+      fixture.detectChanges();
+    });
+
+    it('should create', () => {
+      expect(component).toBeTruthy();
+    });
+
+    it('should contain the default class', () => {
+      expect(
+        fixture.debugElement
+          .query(By.directive(LgLinkMenuItemComponent))
+          .nativeElement.getAttribute('class'),
+      ).toContain('lg-link-menu-item');
+    });
+
+    it('should render the link item component', () => {
+      expect(
+        fixture.debugElement.query(By.directive(LgLinkMenuItemComponent)),
+      ).toBeTruthy();
+    });
+
+    it('should render the internal icon', () => {
+      expect(fixture.debugElement.query(By.css('[name="chevron-right"]'))).toBeDefined();
+      expect(fixture.debugElement.query(By.css('[name="link-external"]'))).toBeNull();
+    });
+
+    it('should render the external icon when the internal input is set to false', () => {
+      component.internal = false;
+      fixture.detectChanges();
+
+      expect(fixture.debugElement.query(By.css('[name="link-external"]'))).toBeDefined();
+      expect(fixture.debugElement.query(By.css('[name="chevron-right"]'))).toBeNull();
+    });
+
+    it('should render the heading', () => {
+      expect(
+        fixture.debugElement.query(By.directive(LgLinkMenuItemHeadingComponent)),
+      ).toBeTruthy();
+    });
+
+    it('should render the content', () => {
+      expect(
+        fixture.debugElement.query(By.directive(LgLinkMenuItemContentComponent)),
+      ).toBeTruthy();
+    });
+
+    it('should log a warning if the parent is not an anchor element', () => {
+      const consoleSpy = spyOn(console, 'warn').and.stub();
+
+      fixture = TestBed.createComponent(TestComponent);
+      component = fixture.componentInstance;
+      fixture.detectChanges();
+
+      expect(consoleSpy).toHaveBeenCalledOnceWith(
+        'expected \'lg-link-menu-item\' parent to be an HTML Anchor but got DIV',
+      );
+    });
   });
 
-  it('should create', () => {
-    expect(component).toBeTruthy();
-  });
+  describe('integration', () => {
+    describe('opens in a new tab text', () => {
+      const createComponentWithTarget = (target: string) => {
+        const template = `
+        <a href="#" [target]="target">
+          <lg-link-menu-item>
+            <lg-link-menu-item-heading>Update my direct debit</lg-link-menu-item-heading>
+            <lg-link-menu-item-content>Do it online</lg-link-menu-item-content>
+          </lg-link-menu-item>
+        </a>
+      `;
 
-  it('should contain the default class', () => {
-    expect(
-      fixture.debugElement
-        .query(By.directive(LgLinkMenuItemComponent))
-        .nativeElement.getAttribute('class'),
-    ).toContain('lg-link-menu-item');
-  });
+        fixture = TestBed.overrideTemplate(TestComponent, template).createComponent(
+          TestComponent,
+        );
 
-  it('should render the link item component', () => {
-    expect(
-      fixture.debugElement.query(By.directive(LgLinkMenuItemComponent)),
-    ).toBeTruthy();
-  });
+        component = fixture.componentInstance;
+        component.target = target;
 
-  it('should render the internal icon', () => {
-    expect(fixture.debugElement.query(By.css('[name="chevron-right"]'))).toBeDefined();
-    expect(fixture.debugElement.query(By.css('[name="link-external"]'))).toBeNull();
-  });
+        fixture.detectChanges();
+      };
 
-  it('should render the external icon when the internal input is set to false', () => {
-    component.internal = false;
-    fixture.detectChanges();
+      const getTextOfElementUnderTest = () =>
+        fixture.debugElement.query(By.css('.lg-link-menu-item__icon-container'))
+          .nativeElement.textContent;
 
-    expect(fixture.debugElement.query(By.css('[name="link-external"]'))).toBeDefined();
-    expect(fixture.debugElement.query(By.css('[name="chevron-right"]'))).toBeNull();
-  });
+      it('should not render the text when the parent anchor target attribute is not "_blank"', () => {
+        createComponentWithTarget('_parent');
 
-  it('should render the heading', () => {
-    expect(
-      fixture.debugElement.query(By.directive(LgLinkMenuItemHeadingComponent)),
-    ).toBeTruthy();
-  });
+        expect(getTextOfElementUnderTest()).toEqual('');
+      });
 
-  it('should render the content', () => {
-    expect(
-      fixture.debugElement.query(By.directive(LgLinkMenuItemContentComponent)),
-    ).toBeTruthy();
+      it('should render the text when the parent anchor target attribute is "_blank"', () => {
+        createComponentWithTarget('_blank');
+
+        expect(getTextOfElementUnderTest()).toEqual(' opens in a new tab');
+      });
+    });
   });
 });
