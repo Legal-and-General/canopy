4883997e4a5c04668158abebd5e86a67e8cd6328
Walter Reis <9714275+Xinchro@users.noreply.github.com>
2023-03-09 10:58:33 +0000

    feat(pipes): improved kebab-case pipe

--- projects/canopy/src/lib/pipes/kebab-case/kebab-case.pipe.spec.ts
+++ projects/canopy/src/lib/pipes/kebab-case/kebab-case.pipe.spec.ts
@@ -16,5 +16,11 @@ describe('LgKebabCasePipe', () => {
   it('should return the input in kebab case', () => {
     expect(pipe.transform('kebab case test')).toBe('kebab-case-test');
     expect(pipe.transform('Kebab Case Test')).toBe('kebab-case-test');
+    expect(pipe.transform('fooBar')).toBe('foo-bar');
+    expect(pipe.transform('fooBarTest')).toBe('foo-bar-test');
+    expect(pipe.transform('__FOO_BAR__')).toBe('foo-bar');
+    expect(pipe.transform('FOO--BAR')).toBe('foo-bar');
+    expect(pipe.transform('foo-Bar_Test_test')).toBe('foo-bar-test-test');
+    expect(pipe.transform('fOo-BaRTest_test')).toBe('f-oo-ba-rtest-test');
   });
 });
--- projects/canopy/src/lib/pipes/kebab-case/kebab-case.pipe.ts
+++ projects/canopy/src/lib/pipes/kebab-case/kebab-case.pipe.ts
@@ -7,6 +7,13 @@ export class LgKebabCasePipe implements PipeTransform {
   transform(str: string): any {
     return !str
       ? str
-      : str.toLowerCase().replace(/\s/g, '-');
+      : str
+        .replace(/[_\-|]{1,}/g, ' ')
+        .replace(/([a-z])([A-Z])/g, (match, group1, group2) => {
+          return `${group1} ${group2.toLowerCase()}`;
+        })
+        .trim()
+        .replace(/ +/g, '-')
+        .toLowerCase();
   }
 }
