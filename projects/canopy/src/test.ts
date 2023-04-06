4e6ec1181e10341a51b84b3a5996b094fce8779b
Andrei Rosu <8149965+AndreiRosu@users.noreply.github.com>
2023-04-06 11:48:26 +0300

    feat(angular-15): upgrade @angular/cli to version 15
    BREAKING CHANGE: upgrade to Angular 15

--- projects/canopy/src/test.ts
+++ projects/canopy/src/test.ts
@@ -9,25 +9,8 @@ import {
   platformBrowserDynamicTesting,
 } from '@angular/platform-browser-dynamic/testing';
 
-declare const require: {
-  context(
-    path: string,
-    deep?: boolean,
-    filter?: RegExp,
-  ): {
-    keys(): Array<string>;
-    <T>(id: string): T;
-  };
-};
-
 // First, initialize the Angular testing environment.
 getTestBed().initTestEnvironment(
   BrowserDynamicTestingModule,
   platformBrowserDynamicTesting(),
 );
-
-// Then we find all the tests.
-const context = require.context('./', true, /\.spec\.ts$/);
-
-// And load the modules.
-context.keys().map(context);
