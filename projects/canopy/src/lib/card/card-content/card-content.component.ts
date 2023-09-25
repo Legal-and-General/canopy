6d03f00b1521a4bcb2f5d273e1acf352e49302cd
Elena Garrone <8397116+elenagarrone@users.noreply.github.com>
2023-09-25 10:05:14 +0100

    feat(card): make links black when inside the card content

--- projects/canopy/src/lib/card/card-content/card-content.component.ts
+++ projects/canopy/src/lib/card/card-content/card-content.component.ts
@@ -3,6 +3,7 @@ import { Component, HostBinding, ViewEncapsulation } from '@angular/core';
 @Component({
   selector: 'lg-card-content',
   templateUrl: './card-content.component.html',
+  styleUrls: [ './card-content.component.scss' ],
   encapsulation: ViewEncapsulation.None,
 })
 export class LgCardContentComponent {
