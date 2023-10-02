4ecae152dc0f71d005ef6fb226246bf7453c4d69
Elena Garrone <8397116+elenagarrone@users.noreply.github.com>
2023-10-02 15:29:13 +0100

    feat(card): revert - make links black when inside the card content
    This reverts commit 6d03f00b1521a4bcb2f5d273e1acf352e49302cd as it
    seems to be a breaking change. Will be added to next branch instead.

--- projects/canopy/src/lib/card/card-content/card-content.component.ts
+++ projects/canopy/src/lib/card/card-content/card-content.component.ts
@@ -3,7 +3,6 @@ import { Component, HostBinding, ViewEncapsulation } from '@angular/core';
 @Component({
   selector: 'lg-card-content',
   templateUrl: './card-content.component.html',
-  styleUrls: [ './card-content.component.scss' ],
   encapsulation: ViewEncapsulation.None,
 })
 export class LgCardContentComponent {
