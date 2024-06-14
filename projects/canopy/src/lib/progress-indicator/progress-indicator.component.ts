a8b26c57b072cce253bcb86094b858e3fb86cc60
Jay Nguyen <132056063+jaynguyenslng@users.noreply.github.com>
2024-06-14 10:26:13 +0100

    feat(progress-bar): update tests and type name

--- projects/canopy/src/lib/progress-indicator/progress-indicator.component.ts
+++ projects/canopy/src/lib/progress-indicator/progress-indicator.component.ts
@@ -7,7 +7,7 @@ import {
 } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
-import { displayAsType } from './progress-indicator.interface';
+import { ProgressDisplayAs } from './progress-indicator.interface';
 import { LgProgressBarComponent } from './progress-bar/progress-bar.component';
 import { LgProgressJourneyComponent } from './progress-journey/progress-journey.component';
 
@@ -27,14 +27,29 @@ export class LgProgressIndicatorComponent {
   @Input() max = 0;
   @Input() value = 0;
   @Input() showProgressBar = true;
-  @Input() displayAs: displayAsType = 'step';
+  @Input() displayAs: ProgressDisplayAs = 'step';
   @Input() stepsPrefix = 'Step';
 
   _id = nextUniqueId++;
+  _ariaLabelledByIds: string;
   _journeyTitleId = `lg-progress-bar-journey-${this._id}`;
   _stepsTextId = `lg-progress-bar-step-${this._id}`;
 
   get ariaLabelledByIds(): string {
-    return `${this._journeyTitleId} ${this._stepsTextId}`;
+    return this._ariaLabelledByIds
+      ? `${this._ariaLabelledByIds}`
+      : `${this._journeyTitleId} ${this._stepsTextId}`;
+  }
+
+  set ariaLabelledByIds(label: string) {
+    this._ariaLabelledByIds = label;
+  }
+
+  set journeyTitleId(value: string) {
+    this._journeyTitleId = value;
+  }
+
+  set stepsTextId(value: string) {
+    this._stepsTextId = value;
   }
 }
