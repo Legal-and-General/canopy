6e3ffde2922e5f9a0448b0030e438860d20bc7e2
Stefan Timosenco <12414812+st3fun1@users.noreply.github.com>
2024-09-10 12:58:37 +0300

    test(ordered list): add unit tests

--- projects/canopy/src/lib/list/list-with-custom-counter/list-with-custom-counter.directive.spec.ts
+++ projects/canopy/src/lib/list/list-with-custom-counter/list-with-custom-counter.directive.spec.ts
@@ -1,1 +1,35 @@
-describe('LgListWithCustomCounter', () => {});
+import { TestBed, waitForAsync } from '@angular/core/testing';
+import { MockedComponentFixture, MockRender } from 'ng-mocks';
+
+import { LgListWithCustomCounterDirective } from './list-with-custom-counter.directive';
+
+describe('LgListWithCustomCounterDirective', () => {
+  let fixture: MockedComponentFixture<LgListWithCustomCounterDirective, {}>;
+  let orderedList: HTMLElement;
+
+  beforeEach(waitForAsync(() => {
+    TestBed.configureTestingModule({
+      imports: [ LgListWithCustomCounterDirective ],
+    }).compileComponents();
+
+    fixture = MockRender(
+      `
+      <ol lgListWithCustomCounter>
+        <li>Item 1</li>
+        <li>Item 2</li>
+      </ol>
+    `,
+      {},
+      { reset: true },
+    );
+
+    orderedList = fixture.debugElement.children[0].nativeElement;
+    fixture.detectChanges();
+  }));
+
+  describe('when the directive is set', () => {
+    it('should add a CSS class to the list', () => {
+      expect(orderedList.getAttribute('class')).toContain('lg-list-with-custom-counter');
+    });
+  });
+});
