922ed51cbe744f3b3e9bcd9fd0a4b936ba73af99
ida-heddingwerner_landg <ida.heddingwerner@landg.com>
2025-10-21 13:20:36 +0100

    docs(brand-modernisation): amend SB welcome page and readme file message

--- README.md
+++ README.md
@@ -5,6 +5,13 @@
 
 # Canopy
 
+> [!WARNING]
+> **We're making big changes to Canopy for L&G's brand modernisation throughout 2025-2026.**
+> 
+> We want to keep you up to date with the planned roll out and give you the support you need.
+> 
+> Please [email us](mailto:CanopyDesignSystem@landg.com?subject=Tell%20me%20about%20Canopy%20updates%20(GH)) and let us know which application or project you are using Canopy for and we'll get in touch.
+
 > An Angular implementation of the Legal & General design system.
 
 Interested in [contributing?](docs/CONTRIBUTING.md)
--- projects/canopy/src/lib/docs/welcome/hero.component.ts
+++ projects/canopy/src/lib/docs/welcome/hero.component.ts
@@ -2,18 +2,12 @@ import { Component } from '@angular/core';
 
 import { LgIconComponent } from '../../icon';
 import { LgMarginDirective } from '../../spacing';
+import { LgAlertComponent } from '../../alert';
 
 @Component({
   selector: 'lg-docs-welcome-hero',
-  imports: [ LgMarginDirective, LgIconComponent ],
+  imports: [ LgMarginDirective, LgIconComponent, LgAlertComponent ],
   template: `
-    <img
-      class="welcome-img"
-      src="docs/welcome/hero.svg"
-      alt="Canopy"
-      lgMarginBottom="lg"
-    />
-
     <h1 class="lg-font--expressive lg-font-size-7--700" lgMarginBottom="sm">
       <span class="welcome-heading no-wrap">Welcome to Canopy,</span>
       <br />
@@ -27,6 +21,47 @@ import { LgMarginDirective } from '../../spacing';
       <span class="lg-visually-hidden"> opens in a new tab</span>
       <lg-icon name="link-external"></lg-icon>
     </a>
+
+    <lg-alert variant="info" lgMarginTop="xl">
+      <p>
+        <strong
+          >Good news! You're viewing the latest version of L&G's digital design language
+          system Canopy as we roll out brand modernisation throughout 2025-2026.</strong
+        >
+      </p>
+      <p>
+        It's easy to see what's been updated already - if the menu item says 'pending'
+        then we haven't got to it yet - but we're working on it. Find our releases
+        <a
+          href="https://github.com/Legal-and-General/canopy/releases?q=&expanded=true"
+          target="_blank"
+          >here</a
+        >.
+      </p>
+      <p>
+        <strong>Need help or want to be kept in the loop?</strong> Please
+        <a
+          href="mailto:CanopyDesignSystem@landg.com?subject=Tell me about Canopy updates (SB)"
+          >email us</a
+        >
+        and let us know which application or project you are using Canopy for and we'll
+        get in touch.
+      </p>
+      <p>
+        <strong>Need to see the legacy version of Canopy?</strong> It's still available
+        <a
+          href="https://legal-and-general.github.io/canopy/lg-sb-legacy/?path=/docs/welcome--docs"
+          target="_blank"
+          >here</a
+        >
+        but we won't be supporting this version in Q2 2026. If you need help to upgrade to
+        the latest version and benefit from all the brand updates please
+        <a
+          href="mailto:CanopyDesignSystem@landg.com?subject=Tell me about Canopy updates (SB)"
+          >get in touch</a
+        >.
+      </p>
+    </lg-alert>
   `,
   styles: [
     `
