869496e3dfe8b696aafa34189f8e851ff4bef4a1
ida-heddingwerner_landg <ida.heddingwerner@landg.com>
2025-10-22 13:17:32 +0100

    docs(brand-modernisation): amend SB welcome page and readme file message

--- README.md
+++ README.md
@@ -5,6 +5,15 @@
 
 # Canopy
 
+> [!WARNING]
+> **This is a legacy version of Canopy.**
+>
+> It is scheduled for deprecation in Q2 2026. Until then, no further dependency updates will be applied to this branch.
+>
+> Please consider using the [latest version of Canopy](https://github.com/Legal-and-General/canopy) instead, which includes the Brand Modernisation updates.
+>
+> We want to keep you up to date with the planned roll out and give you the support you need. Please [email us](mailto:CanopyDesignSystem@landg.com?subject=Tell%20me%20about%20Canopy%20updates%20(GH%20Legacy)) and let us know which application or project you are using Canopy for and we'll get in touch.
+
 > An Angular implementation of the Legal & General design system.
 
 Interested in [contributing?](docs/CONTRIBUTING.md)
--- projects/canopy/src/lib/docs/welcome/hero.component.ts
+++ projects/canopy/src/lib/docs/welcome/hero.component.ts
@@ -2,18 +2,12 @@ import { Component } from '@angular/core';
 
 import { LgIconComponent } from '../../icon';
 import { LgMarginDirective } from '../../spacing';
+import { LgAlertComponent } from '../../alert';
 
 @Component({
   selector: 'lg-docs-welcome-hero',
-  imports: [ LgMarginDirective, LgIconComponent ],
+  imports: [ LgMarginDirective, LgIconComponent, LgAlertComponent ],
   template: `
-    <img
-      class="welcome-img"
-      src="docs/welcome/hero.svg"
-      alt="Canopy"
-      lgMarginBottom="lg"
-    />
-
     <h1 class="lg-font--expressive lg-font-size-7--strong" lgMarginBottom="sm">
       <span class="welcome-heading no-wrap">Welcome to Canopy,</span>
       <br />
@@ -27,6 +21,26 @@ import { LgMarginDirective } from '../../spacing';
       <span class="lg-visually-hidden"> opens in a new tab</span>
       <lg-icon name="link-external"></lg-icon>
     </a>
+
+    <lg-alert variant="info" lgMarginTop="xl">
+      <p>
+        <strong
+          >You're viewing an older version of Canopy that doesn't include L&G's brand
+          modernisation updates.</strong
+        >
+      </p>
+      <p>We won't be supporting this version from Q2 of 2026.</p>
+      <p>
+        We'd recommend you upgrade to the latest version so you can benefit from the new
+        brand as we roll it out over 2025-2026. If you need help to upgrade, or would like
+        to be kept up to date with the roll out, please
+        <a
+          href="mailto:CanopyDesignSystem@landg.com?subject=Tell me about Canopy updates (SB Legacy)"
+          >get in touch</a
+        >
+        and let us know which application or project you are using Canopy for.
+      </p>
+    </lg-alert>
   `,
   styles: [
     `
