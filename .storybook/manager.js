627c19db644f340e996210bb6d6e62bf6752737d
elenagarrone <8397116+elenagarrone@users.noreply.github.com>
2026-01-08 10:03:51 +0000

    docs(badge): fixes issue caused by the upgrade
    The upgrade caused the colour and positioning of the
    guide's badges to show incorrectly.

--- .storybook/manager.js
+++ .storybook/manager.js
@@ -27,29 +27,6 @@ addons.setConfig({
         mdx: true,
       },
     },
-    {
-      tags: 'pending-docs',
-      badge: {
-        text: 'Pending',
-        style: {
-          backgroundColor: '#aee1f7',
-          color: '#1d1d1b',
-          fontWeight: 'normal',
-          fontSize: '10px !important',
-        },
-        tooltip: 'Due for brand modernisation',
-      },
-      display: {
-        sidebar: [
-        {
-            type: 'docs',
-            skipInherited: false,
-        }
-      ],
-        toolbar: true,
-        mdx: true,
-      },
-    },
     ...defaultConfig,
   ],
   theme: canopyTheme
