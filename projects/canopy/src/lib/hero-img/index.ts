faf9a74debeb3a24864ee2fcfd37caf30cb5c169
Anca David <ID+username@users.noreply.github.com>
2026-07-23 09:57:08 +0100

    feat(carousel): deprecate carousel and hero img
    BREAKING CHANGE: Carousel and hero image were removed

--- projects/canopy/src/lib/hero-img/index.ts
+++
@@ -1,4 +0,0 @@
-export * from './hero-img-card/hero-img-card.component';
-export * from './hero-img-card-subtitle/hero-img-card-subtitle.component';
-export * from './hero-img-card-title/hero-img-card-title.component';
-export * from './hero-img.component';
